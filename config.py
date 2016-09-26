9c48b26792b7a5aea13636d29ee41e4c009ee740
Thomas David Baker <bakert@gmail.com>
2016-09-26 08:29:43 +0100

    Add a config.json and code to read it.
    Move token from environment variable into config.
    
    Add image_dir to configure where downloaded images are saved to.

---
+++ config.py
@@ -0,0 +1,18 @@
+import json
+
+class Config:
+  defaults = {
+    "image_dir": "."
+  }
+
+  def __init__(self):
+    try:
+      self.config = json.load(open("config.json"))
+    except FileNotFoundError:
+      self.config = {}
+
+  def get(self, key):
+    if key in self.config:
+      return self.config[key]
+    return Config.defaults[key]
+

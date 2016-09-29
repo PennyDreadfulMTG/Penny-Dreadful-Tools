4893da29d8a2767024bb4ea9866a11e74f17b616
Thomas David Baker <bakert@gmail.com>
2016-09-29 08:17:11 +0100

    Fix the case where the server has no emjois.

--- emoji.py
+++ emoji.py
@@ -7,13 +7,16 @@ db = database.Database()
 def FindEmoji(emoji, channel):
   if channel.is_private:
     return None
-  emojis = channel.server.emojis
-  return next((x for x in emojis if x.name == emoji), None)
+  try:
+    emojis = channel.server.emojis
+    return next((x for x in emojis if x.name == emoji), None)
+  except AttributeError:
+    return None
 
 def ReplaceEmoji(text, channel):
   if channel.is_private:
     return text
-  
+
   output = text
   symbols = re.findall(r'\{([A-Z0-9/]{1,3})\}', text)
   for symbol in symbols:

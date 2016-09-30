ffe3b0a0755276c25fe4f541643b79f694549da7
Thomas David Baker <bakert@gmail.com>
2016-09-30 06:42:29 +0100

    Don't make a breaking change to a perfectly good string.

--- bot.py
+++ bot.py
@@ -201,7 +201,7 @@ async def respond_to_command(message):
         print('Echoing {s}'.format(s=s))
         await STATE.client.send_message(message.channel, s)
     elif message.content.startswith("!barbs"):
-        msg = 'Heroic doesn't get that affected by Barbs. Bogles though. Kills their creature, kills their face.'
+        msg = "Heroic doesn't get that affected by Barbs. Bogles though. Kills their creature, kills their face."
         await STATE.client.send_message(message.channel, msg)
     elif message.content.startswith('!help'):
         msg = """Basic bot usage: Include [cardname] in your regular messages.

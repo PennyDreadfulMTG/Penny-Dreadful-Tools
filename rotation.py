d42593260458fbfc4da6a24e57add215adcbfb17
Thomas David Baker <bakert@gmail.com>
2016-10-21 00:29:26 +0100

    Make !rotation respect Pacific time and display something nicer.

--- rotation.py
+++ rotation.py
@@ -1,14 +1,21 @@
 import datetime
+from dateutil import tz
 import fetcher
 
 def init():
     standard = fetcher.whatsinstandard()
-    return [datetime.datetime.strptime(release["enter_date"], "%Y-%m-%dT%H:%M:%S.%fZ") for release in standard]
+    return [parse_rotation_date(release["enter_date"]) for release in standard]
 
 def last_rotation():
-    return max(d for d in  DATES if d < datetime.datetime.now())
+    return max(d for d in  DATES if d < now())
 
 def next_rotation():
-    return min(d for d in DATES if d > datetime.datetime.now())
+    return min(d for d in DATES if d > now())
+
+def parse_rotation_date(s):
+    return datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=tz.gettz('America/Los_Angeles'))
+
+def now():
+    return datetime.datetime.now().replace(tzinfo=tz.tzlocal())
 
 DATES = init()

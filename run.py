35231e7cd322d1864cf7c618d3110ce76d4cdf65
Thomas David Baker <bakert@gmail.com>
2016-10-26 15:07:43 +0100

    Make run.py work when called via python.

--- run.py
+++ run.py
@@ -1,23 +1,25 @@
 import sys
 
-
 def run():
     if len(sys.argv) == 0:
         print("No entry point specified.")
         exit(1)
 
     app = sys.argv[0]
+    print(app)
 
-    if app == "discordbot":
+    if "discordbot" in sys.argv:
         from discordbot import bot
         bot.init()
-    elif app == "decksite":
+    elif "decksite" in sys.argv:
         from decksite import main
         main.init()
-    elif app == "price_grabber":
+    elif "price_grabber" in sys.argv:
         from price_grabber import price_grabber
         price_grabber.fetch()
         price_grabber.price.cache()
-    elif app == "srv_price":
+    elif "srv_price" in sys.argv:
         from price_grabber import srv_prices
         srv_prices.init()
+
+run()

62bc6dddba76a63bf1bd2ee7f22464e60624ab26
Thomas David Baker <bakert@gmail.com>
2016-10-27 08:23:23 +0100

    Add missing PDException omitted from earlier commit.

---
+++ pd_exception.py
@@ -0,0 +1,11 @@
+class PDException(Exception):
+    pass
+
+class OperationalException(PDException):
+    pass
+
+class ParseException(PDException):
+    pass
+
+class InvalidDataException(PDException):
+    pass

6df07fad7fe83a50703c92fea643cb28ae44c278
Katelyn Gigante <clockwork.singularity@gmail.com>
2016-10-28 04:23:08 +1100

    Add a endpoint to force a pull from tappedout

--- decksite/main.py
+++ decksite/main.py
@@ -16,7 +16,6 @@ def close_db(error):
 def home():
     # Uncomment this to get data for testing. It's slow, though, so probably turn it off against after that.
     # from decksite import tappedout
-    # from magic import configuration
     # if not tappedout.is_authorised():
     #    tappedout.login()
     # tappedout.fetch_decks('penny-dreadful')
@@ -37,6 +36,14 @@ def add_form():
 def add_deck():
     decks.add_deck(request.form)
     return add_form()
+
+@APP.route('/querytappedout')
+def deckcycle_tappedout():
+    from decksite import tappedout
+    if not tappedout.is_authorised():
+        tappedout.login()
+    tappedout.fetch_decks('penny-dreadful')
+    return home()
 
 def init():
     APP.run(host='0.0.0.0', debug=True)

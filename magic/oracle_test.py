54b53ac16429f655d6789217c018c3462b3ca5ee
Thomas David Baker <bakert@gmail.com>
2016-10-29 11:20:41 +0100

    Moar tests.

--- magic/oracle_test.py
+++ magic/oracle_test.py
@@ -8,10 +8,16 @@ def test_legal():
     assert oracle.legal(cards)
     assert oracle.legal(cards, 'Modern')
 
-def test_cards_from_query_split_card():
+def test_cards_from_query():
     cards = oracle.cards_from_query('Far/Away')
     assert len(cards) == 1
     assert cards[0].name == 'Far // Away'
+    cards = oracle.cards_from_query('Jötun Grunt')
+    assert len(cards) == 1
+    assert cards[0].name == 'Jötun Grunt'
+    cards = oracle.cards_from_query('Jotun Grunt')
+    assert len(cards) == 1
+    assert cards[0].name == 'Jötun Grunt'
 
 def test_valid_name():
     assert oracle.valid_name('Dark Ritual') == 'Dark Ritual'

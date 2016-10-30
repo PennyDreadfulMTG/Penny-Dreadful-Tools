c1c8d75410e9495c4c09a3771affbe15926fcef8
Thomas David Baker <bakert@gmail.com>
2016-10-30 09:31:36 +0000

    Allow matching 'j_meka' with 'jmeka' in resources command.
    Fixes #396.

--- discordbot/command_test.py
+++ discordbot/command_test.py
@@ -4,3 +4,5 @@ def test_roughly_matches():
     assert command.roughly_matches('hello', 'hello')
     assert command.roughly_matches('signup', 'Sign Up')
     assert not command.roughly_matches('elephant', 'Tuba')
+    assert command.roughly_matches('jmeka', 'j_meka')
+

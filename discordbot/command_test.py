4a3149ba1af144b572ffc684ffff2e32b5e8d412
Thomas David Baker <bakert@gmail.com>
2016-10-30 10:05:58 +0000

    Remove trailing newline.

--- discordbot/command_test.py
+++ discordbot/command_test.py
@@ -5,4 +5,3 @@ def test_roughly_matches():
     assert command.roughly_matches('signup', 'Sign Up')
     assert not command.roughly_matches('elephant', 'Tuba')
     assert command.roughly_matches('jmeka', 'j_meka')
-

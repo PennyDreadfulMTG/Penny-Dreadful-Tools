3123fcf1f215290c01901c7bba4cf9fbce7282f8
Thomas David Baker <bakert@gmail.com>
2016-11-04 20:12:51 +0000

    I'm not sure where the limit should be but 100 doesn't feel like enough.

--- decksite/data/deck.py
+++ decksite/data/deck.py
@@ -9,7 +9,7 @@ from decksite.data import guarantee, query
 from decksite.database import db
 
 def latest_decks():
-    return load_decks(limit='LIMIT 100')
+    return load_decks(limit='LIMIT 1000')
 
 def load_deck(deck_id):
     return guarantee.exactly_one(load_decks('d.id = {deck_id}'.format(deck_id=sqlescape(deck_id))))

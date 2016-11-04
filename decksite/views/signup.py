912248649f3c852f0778e01defcb3d3fe00848b8
Thomas David Baker <bakert@gmail.com>
2016-11-04 12:51:57 +0000

    Derive sign up titles the same way we do for reporting results.
    There's still some hardcoding of league 2-2 but there's a ticket for that.

--- decksite/views/signup.py
+++ decksite/views/signup.py
@@ -1,9 +1,11 @@
+from decksite import league
 from decksite.view import View
 
 # pylint: disable=no-self-use
 class SignUp(View):
     def __init__(self, form):
         self.form = form
+        self.league = league.active_league()
 
     def subtitle(self):
-        return 'League Sign Up'
+        return '{league} Sign Up'.format(league=self.league['name'])

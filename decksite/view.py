d4f1da2734d1e3e056f543186ba3fb6d99d5049c
Thomas David Baker <bakert@gmail.com>
2016-11-06 12:34:20 +0000

    Show format legality on card page, too.

--- decksite/view.py
+++ decksite/view.py
@@ -95,6 +95,7 @@ class View:
             c.url = url_for('card', name=c.name)
             c.img_url = 'http://magic.bluebones.net/proxies/?c={name}'.format(name=urllib.parse.quote(c.name))
             c.pd_legal = c.legalities.get('Penny Dreadful', False)
+            c.legal_formats = c.legalities.keys()
 
     def prepare_competitions(self):
         for c in getattr(self, 'competitions', []):

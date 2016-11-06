3268c934a2a56b7f28f7fe81d2465044caaa947a
Thomas David Baker <bakert@gmail.com>
2016-11-06 04:24:42 +0000

    Just did this manually in the db instead.

--- decksite/sql/6.sql
+++
@@ -1,4 +0,0 @@
-UPDATE competition SET url = 'https://www.reddit.com/r/PennyDreadfulMTG/comments/50dyzz/penny_dreadful_league/' WHERE name = 'League September 2016';
-UPDATE competition SET url = 'https://www.reddit.com/r/PennyDreadfulMTG/comments/55caky/penny_dreadful_league_season_2/' WHERE name = 'League October 2016';
-UPDATE competition SET url = 'https://www.reddit.com/r/PennyDreadfulMTG/comments/5ah58c/penny_dreadful_league_season_3/' WHERE name = 'League November 2016';
-

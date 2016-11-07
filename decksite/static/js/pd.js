76b6cebf82117e7fea42f4cf43a82b85499eaef3
Thomas David Baker <bakert@gmail.com>
2016-11-07 14:50:52 +0000

    Remove columns we want hidden in HTML not in js.
    The heuristics aren't good enough - we'll make the decision where we know what
    we want.

--- decksite/static/js/pd.js
+++ decksite/static/js/pd.js
@@ -42,33 +42,21 @@ PD.init = function () {
     $('input[type=file]').on('change', PD.loadDeck);
 };
 PD.fadeRepeats = function () {
-    var current, previous, differs;
+    var current, previous;
     $(this).find('td').find('*').addBack().removeClass('repeat');
     $(this).find('tr').each(function() {
         current = $(this).find('td').toArray();
-        if (current.length > 0 && !differs) {
-            differs = new Array(current.length);
-            console.log(current.length);
-        }
         if (previous) {
             for (i = 0; i < current.length; i++) {
                 if ($(current[i]).text().trim() == $(previous[i]).text().trim() && $(current[i]).html().indexOf('mana-') === -1) {
                     if ($(current[i]).text().indexOf('⊕') === -1 && $(current[i]).text().indexOf('★') === -1) {
                         $(current[i]).find('*').addBack().addClass('repeat');
                     }
-                } else {
-                    differs[i] = true;
                 }
             }
         }
         previous = current;
     })
-    for (var i = 0; i < differs.length; i++) {
-        if (!differs[i]) {
-            $(this).find('th:nth-child(' + (i + 1) + ')').hide();
-            $(this).find('td:nth-child(' + (i + 1) + ')').hide();
-        }
-    }
 };
 PD.loadDeck = function () {
     var file = this.files[0],

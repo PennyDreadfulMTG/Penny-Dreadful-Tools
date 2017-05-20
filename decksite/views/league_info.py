d05771b4e1180adbe9bb6912902fe835d4dd95fe
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-05-20 13:07:29 +1000

    Linting

--- decksite/views/league_info.py
+++ decksite/views/league_info.py
@@ -2,10 +2,10 @@ from decksite.view import View
 from decksite import league
 
 def suffix(d):
-    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+    return 'th' if 11 <= d <= 13 else {1:'st', 2:'nd', 3:'rd'}.get(d%10, 'th')
 
-def custom_strftime(format, t):
-    return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+def custom_strftime(fmt, t):
+    return t.strftime(fmt).replace('{S}', str(t.day) + suffix(t.day))
 
 # pylint: disable=no-self-use
 class LeagueInfo(View):

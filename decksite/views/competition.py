c817990fdcffae4ac490e745abe7211c2b7080f8
Thomas David Baker <bakert@gmail.com>
2017-06-06 06:58:16 -0700

    Leaderboard for league points.

--- decksite/views/competition.py
+++ decksite/views/competition.py
@@ -1,3 +1,5 @@
+from flask import url_for
+
 from decksite.view import View
 from decksite import league
 
@@ -7,6 +9,19 @@ class Competition(View):
         self.competition = competition
         self.decks = competition.decks
         self.hide_source = True
+        if competition.type == 'League':
+            leaderboard = {}
+            for d in competition.decks:
+                bonus = 0
+                if d.wins == 5:
+                    bonus = 1
+                points = d.wins + bonus
+                if d.person_id not in leaderboard:
+                    leaderboard[d.person_id] = {'person': d.person, 'url': url_for('person', person_id=d.person_id), 'points': 0}
+                leaderboard[d.person_id]['points'] += points
+            if len(leaderboard) > 0:
+                self.has_leaderboard = True
+                self.leaderboard = sorted(leaderboard.values(), key=lambda k: k['points'], reverse=True)
 
     def __getattr__(self, attr):
         return getattr(self.competition, attr)

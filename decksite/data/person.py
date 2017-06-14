f6ed8ff36d698261a873f27926d5d874478a6afc
Thomas David Baker <bakert@gmail.com>
2017-06-13 19:10:12 -0700

    Show this season stats for people as we do with cards.

--- decksite/data/person.py
+++ decksite/data/person.py
@@ -1,6 +1,8 @@
 from munch import Munch
 
+from magic import rotation
 from shared.database import sqlescape
+
 
 from decksite.data import deck, guarantee, query
 from decksite.database import db
@@ -13,19 +15,29 @@ def load_person_by_username(username):
 
 def load_people(where_clause='1 = 1'):
     sql = """
-        SELECT p.id, {person_query} AS name, COUNT(d.id) AS num_decks,
+        SELECT p.id, {person_query} AS name,
+
+        COUNT(d.id) AS num_decks,
         SUM(d.wins) AS wins,
         SUM(d.losses) AS losses,
         SUM(d.draws) AS draws,
         ROUND((SUM(d.wins) / SUM(d.wins + d.losses)) * 100, 1) AS win_percent,
-        SUM(CASE WHEN d.competition_id IS NOT NULL THEN 1 ELSE 0 END) AS num_competitions
+        SUM(CASE WHEN d.competition_id IS NOT NULL THEN 1 ELSE 0 END) AS num_competitions,
+
+        SUM(CASE WHEN d.created_date >= %s THEN 1 ELSE 0 END) AS num_decks_season,
+        SUM(CASE WHEN d.created_date >= %s THEN wins ELSE 0 END) AS wins_season,
+        SUM(CASE WHEN d.created_date >= %s THEN losses ELSE 0 END) AS losses_season,
+        SUM(CASE WHEN d.created_date >= %s THEN draws ELSE 0 END) AS draws_season,
+        ROUND((SUM(CASE WHEN d.created_date >= %s THEN wins ELSE 0 END) / SUM(CASE WHEN d.created_date >= %s THEN wins ELSE 0 END + CASE WHEN d.created_date >= %s THEN losses ELSE 0 END)) * 100, 1) AS win_percent_season,
+        SUM(CASE WHEN d.created_date >= %s AND d.competition_id IS NOT NULL THEN 1 ELSE 0 END) AS num_competitions_season
+
         FROM person AS p
         LEFT JOIN deck AS d ON p.id = d.person_id
         WHERE {where_clause}
         GROUP BY p.id
-        ORDER BY name
+        ORDER BY num_decks_season DESC, num_decks DESC, name
     """.format(person_query=query.person_query(), where_clause=where_clause)
-    people = [Person(r) for r in db().execute(sql)]
+    people = [Person(r) for r in db().execute(sql, [rotation.last_rotation().timestamp()] * 8)]
     if len(people) > 0:
         set_decks(people)
     return people

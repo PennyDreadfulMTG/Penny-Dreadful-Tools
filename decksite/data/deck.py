25bd82422dabef31f243764aae94c9db75559ed3
Thomas David Baker <bakert@gmail.com>
2017-07-21 17:13:47 +0100

    Lowercase opponent mtgo names.
    Fixes #1122.

--- decksite/data/deck.py
+++ decksite/data/deck.py
@@ -285,7 +285,7 @@ def get_matches(d, should_load_decks=False):
             dm1.games AS game_wins,
             dm2.deck_id AS opponent_deck_id, IFNULL(dm2.games, 0) AS game_losses,
             d2.name AS opponent_deck_name,
-            p.mtgo_username AS opponent
+            {person_query} AS opponent
         FROM `match` AS m
         INNER JOIN deck_match AS dm1 ON m.id = dm1.match_id AND dm1.deck_id = %s
         LEFT JOIN deck_match AS dm2 ON m.id = dm2.match_id AND dm2.deck_id <> %s
@@ -293,7 +293,7 @@ def get_matches(d, should_load_decks=False):
         LEFT JOIN deck AS d2 ON dm2.deck_id = d2.id
         LEFT JOIN person AS p ON p.id = d2.person_id
         ORDER BY round
-    """
+    """.format(person_query=query.person_query())
     matches = [Container(m) for m in db().execute(sql, [d.id, d.id])]
     if should_load_decks and len(matches) > 0:
         decks = load_decks('d.id IN ({ids})'.format(ids=', '.join([sqlescape(str(m.opponent_deck_id)) for m in matches])))

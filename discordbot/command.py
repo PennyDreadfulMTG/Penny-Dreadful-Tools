99994859233c3111b909a99d514bfded37538ba0
Thomas David Baker <bakert@gmail.com>
2017-08-11 19:05:51 +0100

    Better handling of unclosed parentheses and double quotes in searches.

--- discordbot/command.py
+++ discordbot/command.py
@@ -7,6 +7,7 @@ import re
 import subprocess
 import sys
 import time
+import traceback
 
 from typing import List
 
@@ -55,7 +56,7 @@ async def handle_command(message, bot):
                 await method(Commands)
         except Exception as e: # pylint: disable=broad-except
             print('Caught exception processing command `{cmd}`'.format(cmd=message.content))
-            print('{type} {e}'.format(type=type(e), e=e))
+            print(traceback.format_exc())
             await bot.client.send_message(message.channel, 'I know the command `{cmd}` but I could not do that.'.format(cmd=parts[0]))
             await getattr(Commands, 'bug')(Commands, bot, message.channel, 'Command failed with {c}: {cmd}'.format(c=e.__class__.__name__, cmd=message.content), message.author)
     else:

7208f70f8cf0d9cf84a8d9619bdb96d6ab61fc00
Thomas David Baker <bakert@gmail.com>
2017-10-28 08:31:43 -0700

    Tournament hosting instructions.

---
+++ decksite/views/tournament_hosting.py
@@ -0,0 +1,9 @@
+from decksite.view import View
+
+# pylint: disable=no-self-use
+class TournamentHosting(View):
+    def __init__(self):
+        pass
+
+    def subtitle(self):
+        return 'Tournament Hosting'

c0e4ddaff70b949635fe8bef0df9d6c57dee6c1a
Thomas David Baker <bakert@gmail.com>
2017-10-28 21:31:33 -0700

    Link to retire.
    We need to revamp menus but for now at least make it accessible.

--- decksite/views/report.py
+++ decksite/views/report.py
@@ -1,6 +1,12 @@
+from flask import url_for
+
 from decksite.views import LeagueForm
 
 # pylint: disable=no-self-use
 class Report(LeagueForm):
+    def __init__(self, form):
+        super().__init__(form)
+        self.retire_url = url_for('retire')
+
     def subtitle(self):
         return '{league} Result Report'.format(league=self.league['name'])

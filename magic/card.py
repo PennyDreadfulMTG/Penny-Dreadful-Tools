09796874fe5fb951711721d43bdc2f44e5e99add
Thomas David Baker <bakert@gmail.com>
2017-11-05 21:04:48 -0800

    Fix idiotic typo.

--- magic/card.py
+++ magic/card.py
@@ -170,7 +170,7 @@ def card_bugs_properties():
     props['id']['primary_key'] = True
     props['card_id']['type'] = INTEGER
     props['card_id']['foreign_key'] = ('card', 'id')
-    props['description']['description'] = TEXT
+    props['description']['type'] = TEXT
     props['last_confirmed']['type'] = INTEGER
     return props
 

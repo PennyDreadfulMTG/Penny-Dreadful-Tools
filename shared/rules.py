7f132ff5e62865db45875baeb2d156f369de0f52
Thomas David Baker <bakert@gmail.com>
2017-11-10 16:52:00 -0800

    Simplify what the bot says for !explain bugs and link from bugs page.

--- shared/rules.py
+++ shared/rules.py
@@ -17,14 +17,6 @@ def bugs(version=LONG, fmt=TEXT):
     s = """
             We allow the playing of cards with known bugs in Penny Dreadful with specific conditions.
 
-            Cards with game-breaking bugs should not be played.
-
-            Cards with disadvantageous bugs can be played and no extra rules apply. The opposing player is under no obligation to treat the card as if it worked properly.
-
-            Cards with advantageous bugs can be played but accruing advantage intentionally will result in disqualification.
-
-            Accruing advantage any other way with a card with an advantageous bug is a game loss for the owner of the bugged card.
-
     """
     if version == SHORT:
         s += """
@@ -33,6 +25,14 @@ def bugs(version=LONG, fmt=TEXT):
         """
     elif version == LONG:
         s += """
+            * Cards with game-breaking bugs should not be played.
+
+            * Cards with disadvantageous bugs can be played and no extra rules apply. The opposing player is under no obligation to treat the card as if it worked properly.
+
+            * Cards with advantageous bugs can be played but accruing advantage intentionally will result in disqualification.
+
+            * Accruing advantage any other way with a card with an advantageous bug is a game loss for the owner of the bugged card.
+
             **Example of Game Loss**: Playing Living Lore with two cards in graveyard and opponent removes one at instant speed with a card from their hand forcing the Living Lore player to imprint a split card.
 
             **Second example of Game Loss**: Playing Profane Command using the mode that targets an opponent. Opponent plays Gilded Light in response.
@@ -44,6 +44,10 @@ def bugs(version=LONG, fmt=TEXT):
             The game loss should be enacted by the bugged cards controller conceding the game.
 
             Any confusion should be discussed with the Tournament Organizer before conceding the game or ending the match, on the bugged card player's clock.
+
+            * When a new bug is encountered for the first time (does not appear at <https://pennydreadfulmagic.com/bugs/>) during a competitive match both players may agree to play on.
+
+            * If either player does not agree the player with the bugged cards must swap out the bugged cards (only) from their decklist, restart the match, players concede games in the correct order until the record matches the record before the bugged game. Sideboarding then takes place if it was not game 1 and the rest of the match plays out.
 
             For all these matters the Tournament Organizer has the flexibility to rule as they see fit and their decision is final.
 

bd53c5b61572e5ceebad8cff4ffe182afdfe4250
Thomas David Baker <bakert@gmail.com>
2017-11-13 00:27:05 -0800

    Get a bit more liberal in !resources/!pdm matching. WIll see how it goes.
    Fixes #1481.

--- discordbot/command_test.py
+++ discordbot/command_test.py
@@ -5,3 +5,4 @@ def test_roughly_matches():
     assert command.roughly_matches('signup', 'Sign Up')
     assert not command.roughly_matches('elephant', 'Tuba')
     assert command.roughly_matches('jmeka', 'j_meka')
+    assert command.roughly_matches('modo bugs', 'modo-bugs')

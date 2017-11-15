e53993af93c2bd6f6b1a8814b19f6b89b08f239f
Thomas David Baker <bakert@gmail.com>
2017-11-14 21:50:22 -0800

    Don't treat strings as lists in perf output.

--- shared/perf.py
+++ shared/perf.py
@@ -10,7 +10,8 @@ def start():
 def check(start_time, kind, detail, location):
     run_time = time.perf_counter() - start_time
     limit = configuration.get(kind)
+    detail_s = detail if isinstance(detail, str) else '; '.join(map(str, list(detail)))
     if limit is not None and run_time > limit:
-        msg = 'Exceeded {kind} limit ({run_time} > {limit}) in {location}: {detail} ({kind}, {run_time}, {location})'.format(kind=kind, run_time=round(run_time, 1), limit=limit, detail='; '.join(map(str, list(detail))), location=location)
+        msg = 'Exceeded {kind} limit ({run_time} > {limit}) in {location}: {detail_s} ({kind}, {run_time}, {location})'.format(kind=kind, run_time=round(run_time, 1), limit=limit, detail_s=detail_s, location=location)
         print(msg)
         fetcher.create_github_issue(msg, 'perf', location)

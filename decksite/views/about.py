1f610b2e2d2ae1b13a903449fc5c2c3a00606941
Thomas David Baker <bakert@gmail.com>
2017-11-18 16:43:40 -0800

    Don't load fancy cards on import just when we use the template.

--- decksite/views/about.py
+++ decksite/views/about.py
@@ -5,41 +5,6 @@ from flask import url_for
 from decksite.view import View
 from magic import legality, oracle
 
-FANCY_CARDS = legality.cards_legal_in_format(oracle.load_cards([
-    'Mother of Runes',
-    'Treasure Cruise',
-    'Hymn to Tourach',
-    'Hermit Druid',
-    'Frantic Search',
-    'Necropotence',
-    'Tendrils of Agony',
-    'Hypergenesis',
-    "Mind's Desire",
-    'Recurring Nightmare',
-    'Worldgorger Dragon',
-    'Astral Slide',
-    'Dark Ritual',
-    'Fact or Fiction',
-    'High Tide',
-    "Nevinyrral's Disk",
-    'Lake of the Dead',
-    'Braids, Cabal Minion',
-    'Channel',
-    'Chain Lightning',
-    'Brain Freeze',
-    'Dragonstorm',
-    'Day of Judgment',
-    'Cruel Ultimatum',
-    'Rofellos, Llanowar Emissary',
-    'Mana Leak',
-    'Burning of Xinye',
-    'Psychatog',
-    'Smokestack',
-    'Llanowar Elves',
-    'Isamaru, Hound of Konda',
-    'Animate Dead'
-]), 'Penny Dreadful')
-
 # pylint: disable=no-self-use
 class About(View):
     def __init__(self,  src):
@@ -51,5 +16,42 @@ class About(View):
         return 'About Penny Dreadful'
 
     def exciting_cards(self):
-        random.shuffle(FANCY_CARDS)
-        return FANCY_CARDS[:3]
+        cards = fancy_cards()
+        random.shuffle(cards)
+        return cards[:3]
+
+def fancy_cards():
+    return legality.cards_legal_in_format(oracle.load_cards([
+        'Mother of Runes',
+        'Treasure Cruise',
+        'Hymn to Tourach',
+        'Hermit Druid',
+        'Frantic Search',
+        'Necropotence',
+        'Tendrils of Agony',
+        'Hypergenesis',
+        "Mind's Desire",
+        'Recurring Nightmare',
+        'Worldgorger Dragon',
+        'Astral Slide',
+        'Dark Ritual',
+        'Fact or Fiction',
+        'High Tide',
+        "Nevinyrral's Disk",
+        'Lake of the Dead',
+        'Braids, Cabal Minion',
+        'Channel',
+        'Chain Lightning',
+        'Brain Freeze',
+        'Dragonstorm',
+        'Day of Judgment',
+        'Cruel Ultimatum',
+        'Rofellos, Llanowar Emissary',
+        'Mana Leak',
+        'Burning of Xinye',
+        'Psychatog',
+        'Smokestack',
+        'Llanowar Elves',
+        'Isamaru, Hound of Konda',
+        'Animate Dead'
+    ]), 'Penny Dreadful')

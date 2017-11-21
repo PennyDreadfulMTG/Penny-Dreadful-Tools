3016263847cf212eece4e59e277c0fd33dc0c63a
Thomas David Baker <bakert@gmail.com>
2017-11-21 06:27:51 -0800

    Approx 50% speedup on cards query by adding correct index and removing subquery.
    Without the ORDER BY the EXPLAIN for this looks really good but in practical
    terms the ORDER BY only seems to cost tens of ms (even though it adds using
    temporary and using filesort to the EXPLAIN).

--- decksite/data/card.py
+++ decksite/data/card.py
@@ -10,49 +10,30 @@ def played_cards(where='1 = 1'):
         SELECT
             card AS name,
             COUNT(card) AS `all_num_decks`,
-            SUM(CASE WHEN maindeck_n > 0 THEN 1 ELSE 0 END) AS `all_n_maindecks`,
-            SUM(CASE WHEN sideboard_n > 0 THEN 1 ELSE 0 END) AS `all_n_sideboards`,
-            SUM(maindeck_n + sideboard_n) AS `all_count_decks`,
-            SUM(maindeck_n) AS `all_count_maindecks`,
-            SUM(sideboard_n) AS `all_count_sideboards`,
+            SUM(CASE WHEN NOT sideboard THEN 1 ELSE 0 END) AS `all_n_maindecks`,
+            SUM(CASE WHEN sideboard THEN 1 ELSE 0 END) AS `all_n_sideboards`,
+            SUM(n) AS `all_count_decks`,
+            SUM(CASE WHEN NOT sideboard THEN n ELSE 0 END) AS `all_count_maindecks`,
+            SUM(CASE WHEN sideboard THEN n ELSE 0 END) AS `all_count_sideboards`,
             SUM(wins) AS `all_wins`,
             SUM(losses) AS `all_losses`,
             SUM(draws) AS `all_draws`,
             IFNULL(ROUND((SUM(wins) / SUM(wins + losses)) * 100, 1), '') AS `all_win_percent`,
 
             SUM(CASE WHEN created_date >= %s THEN 1 ELSE 0 END) AS `season_num_decks`,
-            SUM(CASE WHEN created_date >= %s AND maindeck_n > 0 THEN 1 ELSE 0 END) AS `season_n_maindecks`,
-            SUM(CASE WHEN created_date >= %s AND sideboard_n > 0 THEN 1 ELSE 0 END) AS `season_n_sideboards`,
-            SUM(CASE WHEN created_date >= %s THEN maindeck_n + sideboard_n ELSE 0 END) AS `season_count_decks`,
-            SUM(CASE WHEN created_date >= %s THEN maindeck_n ELSE 0 END) AS `season_count_maindecks`,
-            SUM(CASE WHEN created_date >= %s THEN sideboard_n ELSE 0 END) AS `season_count_sideboards`,
+            SUM(CASE WHEN created_date >= %s AND NOT sideboard THEN 1 ELSE 0 END) AS `season_n_maindecks`,
+            SUM(CASE WHEN created_date >= %s AND sideboard THEN 1 ELSE 0 END) AS `season_n_sideboards`,
+            SUM(CASE WHEN created_date >= %s THEN n ELSE 0 END) AS `season_count_decks`,
+            SUM(CASE WHEN created_date >= %s AND NOT sideboard THEN n ELSE 0 END) AS `season_count_maindecks`,
+            SUM(CASE WHEN created_date >= %s AND sideboard THEN n ELSE 0 END) AS `season_count_sideboards`,
             SUM(CASE WHEN created_date >= %s THEN wins ELSE 0 END) AS `season_wins`,
             SUM(CASE WHEN created_date >= %s THEN losses ELSE 0 END) AS `season_losses`,
             SUM(CASE WHEN created_date >= %s THEN draws ELSE 0 END) AS `season_draws`,
             ROUND((SUM(CASE WHEN created_date >= %s THEN wins ELSE 0 END) / SUM(CASE WHEN created_date >= %s THEN wins ELSE 0 END + CASE WHEN created_date >= %s THEN losses ELSE 0 END)) * 100, 1) AS `season_win_percent`
-        FROM (
-            SELECT
-                d.created_date,
-                d.person_id,
-                d.wins,
-                d.draws,
-                d.losses,
-                card,
-                maindeck_n,
-                sideboard_n
-            FROM (
-                    SELECT
-                        card,
-                        deck_id,
-                        SUM(CASE WHEN NOT sideboard THEN n ELSE 0 END) AS maindeck_n,
-                        SUM(CASE WHEN sideboard THEN n ELSE 0 END) AS sideboard_n
-                    FROM deck_card
-                    GROUP BY deck_id, card
-            ) AS dc
-            LEFT JOIN deck AS d ON d.id = dc.deck_id
-            WHERE {where}
-        ) AS deck_card_agg
-        GROUP BY card
+        FROM deck_card AS dc
+        LEFT JOIN deck AS d ON d.id = dc.deck_id
+        WHERE 1 = 1
+        GROUP BY dc.card
         ORDER BY `season_num_decks` DESC, `season_count_decks` DESC, `season_n_maindecks` DESC, `season_count_maindecks` DESC, `all_num_decks` DESC, `all_count_decks` DESC, `all_n_maindecks` DESC, `all_count_maindecks` DESC
     """.format(where=where)
     cs = [Container(r) for r in db().execute(sql, [int(rotation.last_rotation().timestamp())] * 12)]

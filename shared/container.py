3f35b0e8bdd0f76c6b541535d570268c32f7d180
Thomas David Baker <bakert@gmail.com>
2017-11-20 19:34:53 -0800

    Hack Container property access for 400ms on /cards/Swamp/ and more on /cards/.

--- shared/container.py
+++ shared/container.py
@@ -2,4 +2,12 @@ from munch import Munch
 
 #pylint: disable=too-many-instance-attributes
 class Container(Munch):
-    pass
+    # Reverse the order of operations from Munch because it gives us a speedup when we access hundreds of thousands of properties in a request.
+    def __getattr__(self, k):
+        try:
+            return self[k]
+        except KeyError:
+            try:
+                return object.__getattribute__(self, k)
+            except AttributeError:
+                raise AttributeError(k)

e76505b786e510edcbbf3db558ce7b503465c181
Thomas David Baker <bakert@gmail.com>
2017-12-01 11:47:03 -0800

    Improve league name generation.

--- decksite/league_test.py
+++ decksite/league_test.py
@@ -8,3 +8,7 @@ def test_determine_end_of_league():
     start_date = dtutil.parse('2017-10-31 11:59:59.999', '%Y-%m-%d %H:%M:%S.%f', dtutil.WOTC_TZ)
     end_date = league.determine_end_of_league(start_date)
     assert dtutil.dt2ts(end_date) == 1512115199
+
+def test_determine_league_name():
+    end_date = dtutil.ts2dt(1512115199).astimezone(dtutil.WOTC_TZ)
+    assert league.determine_league_name(end_date) == 'League November 2017'

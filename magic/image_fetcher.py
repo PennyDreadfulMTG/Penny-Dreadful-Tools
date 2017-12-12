35100302555ea3303f1bb3ddc3ad401846b4ebde
Thomas David Baker <bakert@gmail.com>
2017-12-12 14:43:37 -0800

    !art command and refactor command/image_fetcher to make it sane.
    Fixes #615.

--- magic/image_fetcher.py
+++ magic/image_fetcher.py
@@ -18,8 +18,11 @@ def bluebones_image(cards) -> str:
     c = '|'.join(card.name for card in cards)
     return 'http://magic.bluebones.net/proxies/index2.php?c={c}'.format(c=escape(c))
 
-def scryfall_image(card) -> str:
-    return "https://api.scryfall.com/cards/named?exact={c}&format=image".format(c=escape(card.name))
+def scryfall_image(card, version='') -> str:
+    u = "https://api.scryfall.com/cards/named?exact={c}&format=image".format(c=escape(card.name))
+    if version:
+        u += '&version={v}'.format(v=escape(version))
+    return u
 
 def mci_image(printing) -> str:
     return "http://magiccards.info/scans/en/{code}/{number}.jpg".format(code=printing.set_code.lower(), number=printing.number)
@@ -29,18 +32,7 @@ def gatherer_image(printing) -> str:
     if multiverse_id and int(multiverse_id) > 0:
         return 'https://image.deckbrew.com/mtg/multiverseid/'+ str(multiverse_id) + '.jpg'
 
-def download_image(cards) -> str:
-    # helper functions:
-
-    imagename = basename(cards)
-    # Hash the filename if it's otherwise going to be too large to use.
-    if len(imagename) > 240:
-        imagename = hashlib.md5(imagename.encode('utf-8')).hexdigest()
-    filename = imagename + '.jpg'
-    filepath = '{dir}/{filename}'.format(dir=configuration.get('image_dir'), filename=filename)
-    if internal.acceptable_file(filepath):
-        return filepath
-
+def download_bluebones_image(cards, filepath):
     print('Trying to get image for {cards}'.format(cards=', '.join(card.name for card in cards)))
     try:
         internal.store(bluebones_image(cards), filepath)
@@ -49,14 +41,16 @@ def download_image(cards) -> str:
     if internal.acceptable_file(filepath):
         return filepath
 
+def download_scryfall_image(cards, filepath, version='') -> str:
     print('Trying to get scryfall image for {card}'.format(card=cards[0]))
     try:
-        internal.store(scryfall_image(cards[0]), filepath)
+        internal.store(scryfall_image(cards[0], version=version), filepath)
     except FetchException as e:
         print('Error: {e}'.format(e=e))
     if internal.acceptable_file(filepath):
         return filepath
 
+def download_mci_image(cards, filepath) -> str:
     printings = oracle.get_printings(cards[0])
     for p in printings:
         print("Trying to get MCI image for {imagename}".format(imagename=imagename))
@@ -73,4 +67,23 @@ def download_image(cards) -> str:
                 return filepath
         except FetchException as e:
             print('Error: {e}'.format(e=e))
+
+def filepath(cards) -> str:
+    imagename = basename(cards)
+    # Hash the filename if it's otherwise going to be too large to use.
+    if len(imagename) > 240:
+        imagename = hashlib.md5(imagename.encode('utf-8')).hexdigest()
+    filename = imagename + '.jpg'
+    return '{dir}/{filename}'.format(dir=configuration.get('image_dir'), filename=filename)
+
+def download_image(cards) -> str:
+    fp = filepath(cards)
+    if internal.acceptable_file(fp):
+        return fp
+    if download_bluebones_image(cards, fp):
+        return fp
+    if download_scryfall_image(cards, fp):
+        return fp
+    if download_mci_image(cards, fp):
+        return fp
     return None

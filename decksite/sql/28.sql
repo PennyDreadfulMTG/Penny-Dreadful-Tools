ffa3316e36857fc40672051d28f630a6ebe0031c
Thomas David Baker <bakert@gmail.com>
2017-12-26 23:52:43 +0000

    Don't try and change an fk column, make it an fk later instead.

--- decksite/sql/28.sql
+++ decksite/sql/28.sql
@@ -16,7 +16,6 @@ INSERT INTO competition_series (name, competition_type_id) VALUES
     ('Penny Dreadful Saturdays', 2);
 
 ALTER TABLE competition ADD COLUMN competition_series_id INT;
-ALTER TABLE competition ADD FOREIGN KEY (competition_series_id) REFERENCES competition_series (id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 UPDATE competition SET competition_series_id = 1 WHERE name LIKE '%%League%%';
 UPDATE competition SET competition_series_id = 2 WHERE name LIKE '%%Thursday%%' OR name LIKE '%%PDT%%';
@@ -29,5 +28,6 @@ ALTER TABLE competition DROP FOREIGN KEY competition_ibfk_1;
 ALTER TABLE competition DROP COLUMN competition_type_id; -- Now derivable from competition_series_id
 
 ALTER TABLE competition CHANGE COLUMN competition_series_id competition_series_id INT NOT NULL;
+ALTER TABLE competition ADD FOREIGN KEY (competition_series_id) REFERENCES competition_series (id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 COMMIT;

d75b541a77e2ae73813ff9364d9cbed73ec4e5c6
Thomas David Baker <bakert@gmail.com>
2017-12-31 00:39:25 +0000

    Nicer visuals for the tournament page. Show upcoming tourneys in calendar.

--- decksite/views/tournaments.py
+++ decksite/views/tournaments.py
@@ -4,6 +4,7 @@ from flask import url_for
 from magic import tournaments
 from shared import rules
 
+from decksite.data import competition
 from decksite.view import View
 
 # pylint: disable=no-self-use
@@ -15,7 +16,24 @@ class Tournaments(View):
         self.next_tournament_time = info['next_tournament_time']
         self.leaderboards_url = url_for('tournament_leaderboards')
 
-        self.tournaments = tournaments.all_series_info()
+        self.tournaments = sorted(tournaments.all_series_info(), key=lambda t: t.time)
+        leagues = competition.load_competitions("c.competition_series_id IN (SELECT id FROM competition_series WHERE name = 'League') AND c.end_date > UNIX_TIMESTAMP(NOW())")
+        end_date, prev_month, shown_end = None, None, False
+        for t in self.tournaments:
+            month = t.time.strftime('%b')
+            if month != prev_month:
+                t.month = month
+                prev_month = month
+            t.date = t.time.strftime('%-d')
+            if len(leagues) > 0 and t.time >= leagues[-1].start_date and t.time < leagues[-1].end_date:
+                t.league = leagues.pop(-1)
+                t.league.display = True
+                end_date = t.league.end_date
+            elif not shown_end and end_date and t.time >= end_date:
+                t.league = {'class': 'begin', 'display': False}
+                shown_end = True
+            elif end_date:
+                t.league = {'class': 'ongoing', 'display': False}
         p = inflect.engine()
         self.num_tournaments = p.number_to_words(len(self.tournaments))
 

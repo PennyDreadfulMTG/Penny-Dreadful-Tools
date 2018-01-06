e0b02de5bb9e30a974f7a8e8dac5a763e9be1417
Jesus Gabriel y Galan <jgabrielygalan@gmail.com>
2018-01-06 22:19:32 +0100

    decksite: fix person + lint warnings

--- decksite/main.py
+++ decksite/main.py
@@ -37,7 +37,7 @@ def decks():
 @auth.logged
 def deck(deck_id):
     d = ds.load_deck(deck_id)
-    if auth.discord_id() and auth.logged_person() is None:
+    if auth.discord_id() and auth.logged_person() is None and not d.is_person_associated():
         ps.associate(d, auth.discord_id())
         p = ps.load_person_by_discord_id(auth.discord_id())
         auth.log_person(p.id, p.name)

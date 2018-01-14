91066b7bb744c3af7149e19df3c1885b9ccdfec7
Thomas David Baker <bakert@gmail.com>
2018-01-14 18:30:14 +0000

    Interestingness highlighting for rotation tracking list.
    Fixes #4014.

--- decksite/views/rotation_changes.py
+++ decksite/views/rotation_changes.py
@@ -1,30 +1,18 @@
 from decksite.view import View
+from magic import rotation
 
 # pylint: disable=no-self-use
 class RotationChanges(View):
     def __init__(self, cards_in, cards_out, playability, speculation=False):
-        self.playability = playability
-        print(playability.keys())
         self.sections = []
         self.cards = cards_in + cards_out
-        self.speculation = speculation
-        entries_in = [{'name': c.name, 'card': c, 'interestingness': self.interesting(c)} for c in cards_in]
-        entries_out = [{'name': c.name, 'card': c, 'interestingness': self.interesting(c, new=False)} for c in cards_out]
+        entries_in = [{'name': c.name, 'card': c, 'interestingness': rotation.interesting(playability, c, speculation)} for c in cards_in]
+        entries_out = [{'name': c.name, 'card': c, 'interestingness': rotation.interesting(playability, c, speculation, new=False)} for c in cards_out]
         self.sections.append({'name': 'New this season', 'entries': entries_in, 'num_entries': len(entries_in)})
         self.sections.append({'name': 'Rotated out', 'entries': entries_out, 'num_entries': len(entries_out)})
         self.speculation = speculation
 
     def subtitle(self):
         if self.speculation:
-            return "Rotation speculation: what rotation would look like if it happened today"
-        return "Rotation Changes"
-
-    def interesting(self, c, new=True):
-        if new and len({k: v for (k, v) in c['legalities'].items() if 'Penny Dreadful' in k}) == (0 if self.speculation else 1):
-            return 'new'
-        p = self.playability.get(c.name, 0)
-        if p > 0.1:
-            return 'heavily-played'
-        elif p > 0.01:
-            return 'moderately-played'
-        return None
+            return 'Rotation Speculation'
+        return 'Rotation Changes'

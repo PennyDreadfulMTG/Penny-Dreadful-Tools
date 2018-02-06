79d4a6cec7d50c44350b5bbc02755430f086bb3e
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-02-07 01:50:33 +1100

    Translation Keys for the Signup Form

--- decksite/views/signup.py
+++ decksite/views/signup.py
@@ -1,6 +1,26 @@
+from flask_babel import gettext
+
 from decksite.views import LeagueForm
 
 # pylint: disable=no-self-use
 class SignUp(LeagueForm):
     def subtitle(self):
         return '{league} Sign Up'.format(league=self.league['name'])
+
+    def TT_MTGOTRADERS_SIGNUP_TIK(self):
+        return gettext('When you complete a five match league run for the first time ever you will get 1 tik credit with MTGO Traders.')
+
+    def TT_MTGO_USERNAME(self):
+        return gettext('Magic Online Username')
+
+    def TT_DECK_NAME(self):
+        return gettext('Deck Name')
+
+    def TT_DECKLIST(self):
+        return gettext('Decklist')
+
+    def TT_ENTER_OR_UPLOAD(self):
+        return gettext('Enter or upload your decklist')
+
+    def TT_SIGNUP(self):
+        return gettext('Sign Up')

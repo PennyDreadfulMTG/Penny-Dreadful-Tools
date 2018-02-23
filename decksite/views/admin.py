44ee1caed17c452ce95d59573e50237274a47aad
Thomas David Baker <bakert@gmail.com>
2018-02-23 17:52:45 +0000

    Admin menu (show via js when authenticated).

--- decksite/views/admin.py
+++ decksite/views/admin.py
@@ -3,9 +3,8 @@ from decksite.view import View
 
 # pylint: disable=no-self-use
 class Admin(View):
-    def __init__(self, urls):
-        self.urls = urls
-
+    def __init__(self, menu):
+        self.menu = menu
 
     def subtitle(self):
         return 'Admin Menu'

1924c6c527dff2a590089375f5d64efbceba55a9
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-03-29 11:26:04 +1100

    Tappedout is blocking requests from Travis IPs.

--- decksite/scrapers/scraper_test.py
+++ decksite/scrapers/scraper_test.py
@@ -5,6 +5,7 @@ from decksite.scrapers import gatherling, tappedout
 
 
 @pytest.mark.slowtest
+@pytest.mark.tappedout
 def test_tappedout():
     prev = APP.config["SERVER_NAME"]
     APP.config["SERVER_NAME"] = "127:0.0.1:5000"
@@ -13,10 +14,12 @@ def test_tappedout():
     APP.config["SERVER_NAME"] = prev
 
 @pytest.mark.slowtest
+@pytest.mark.gatherling
 def test_gatherling():
     with APP.app_context():
         gatherling.scrape(5)
 
+@pytest.mark.tappedout
 def test_manual_tappedout():
     with APP.app_context():
         tappedout.scrape_url('https://tappedout.net/mtg-decks/60-island/') # Best deck

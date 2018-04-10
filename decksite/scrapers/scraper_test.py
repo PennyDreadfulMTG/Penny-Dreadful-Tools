67eb08befa8491cd2333a811605cf7be85a76192
Thomas David Baker <bakert@gmail.com>
2018-04-10 14:12:35 +0100

    Reinstate tapped out tests but mark them as slow to avoid running  during dev.

--- decksite/scrapers/scraper_test.py
+++ decksite/scrapers/scraper_test.py
@@ -7,12 +7,11 @@ from decksite.scrapers import gatherling, tappedout
 @pytest.mark.slowtest
 @pytest.mark.tappedout
 def test_tappedout():
-    # prev = APP.config["SERVER_NAME"]
-    # APP.config["SERVER_NAME"] = "127:0.0.1:5000"
-    # with APP.app_context():
-    #     tappedout.scrape()
-    # APP.config["SERVER_NAME"] = prev
-    pass
+    prev = APP.config["SERVER_NAME"]
+    APP.config["SERVER_NAME"] = "127:0.0.1:5000"
+    with APP.app_context():
+        tappedout.scrape()
+    APP.config["SERVER_NAME"] = prev
 
 @pytest.mark.slowtest
 @pytest.mark.gatherling
@@ -20,9 +19,8 @@ def test_gatherling():
     with APP.app_context():
         gatherling.scrape(5)
 
+@pytest.mark.slowtest
 @pytest.mark.tappedout
 def test_manual_tappedout():
-    # with APP.app_context():
-    #     tappedout.scrape_url('https://tappedout.net/mtg-decks/60-island/') # Best deck
-    if __name__ == 'false':
-        tappedout.scrape()
+    with APP.app_context():
+        tappedout.scrape_url('https://tappedout.net/mtg-decks/60-island/') # Best deck

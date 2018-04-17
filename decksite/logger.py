f12666e84ff1d3e494235a56a680a6bc1614b3c8
Thomas David Baker <bakert@gmail.com>
2018-04-17 14:12:29 +0200

    Handle multiple arguments to logger in a simple way.
    I think it's better to be able to pass multiple vars than it is to follow  the
    standard logging signature which I don't anticipate needing.

--- decksite/logger.py
+++ decksite/logger.py
@@ -1,22 +1,23 @@
 from decksite import APP
+from typing import Any, List
 
 
-def fatal(*args):
+def fatal(*args: List[Any]) -> None:
     """Panic stations! Data is being irretrievably lost or other truly terrible things."""
-    APP.logger.fatal(*args)
+    APP.logger.fatal('\n'.join(map(str, args)))
 
-def error(*args):
+def error(*args: Any) -> None:
     """Thing that should not happen or state that should not occur, must be fixed."""
-    APP.logger.error(*args)
+    APP.logger.error('\n'.join(map(str, args)))
 
-def warn(*args):
+def warn(*args: Any) -> None:
     """Potentially interesting information that will be logged in production."""
-    APP.logger.warn(*args)
+    APP.logger.warn('\n'.join(map(str, args)))
 
-def info(*args):
+def info(*args: Any) -> None:
     """Potentially interesting information that will not be logged in production but will be logged in dev."""
-    APP.logger.info(*args)
+    APP.logger.info('\n'.join(map(str, args)))
 
-def debug(*args):
+def debug(*args: Any) -> None:
     """Anything that might potentially be useful debugging an issue but not worth showing all the time, even in dev."""
-    APP.logger.debug(*args)
+    APP.logger.debug('\n'.join(map(str, args)))

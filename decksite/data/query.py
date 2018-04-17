5140e7f523d2f3099d1676b05515641e5b09b637
Thomas David Baker <bakert@gmail.com>
2018-04-17 16:50:34 +0200

    Standardize on season_id being an int only.

--- decksite/data/query.py
+++ decksite/data/query.py
@@ -1,5 +1,4 @@
-from magic import rotation
-from shared.database import sqlescape
+from shared.pd_exception import InvalidArgumentException
 
 
 def person_query(table='p'):
@@ -44,11 +43,11 @@ def competition_join():
             competition_type AS ct ON ct.id = cs.competition_type_id
     """
 
-def season_query(season=None):
-    if season is None or season == 'all':
+def season_query(season_id=None):
+    if season_id is None or season_id == 'all':
         return 'TRUE'
     try:
-        return 'season.id = {season_id}'.format(season_id=int(season))
+        return 'season.id = {season_id}'.format(season_id=int(season_id))
     except ValueError:
         raise InvalidArgumentException('No season with id `{season_id}`'.format(season_id=season_id))
 

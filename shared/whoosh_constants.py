7bf7d7ad2813ea171a407b9731120df6364b1b90
Thomas David Baker <bakert@gmail.com>
2018-04-19 09:00:56 +0200

    Standardize quotes across the whole repo (13/N).

--- shared/whoosh_constants.py
+++ shared/whoosh_constants.py
@@ -7,5 +7,5 @@ from shared import configuration
 class WhooshConstants():
     index_dir = configuration.get('whoosh_index_dir')
     tokenized_analyzer = StandardAnalyzer(stoplist=None)
-    normalized_analyzer = IDTokenizer() | SubstitutionFilter(r"[\s/,_'-]", "") | LowercaseFilter()
+    normalized_analyzer = IDTokenizer() | SubstitutionFilter(r"[\s/,_'-]", '') | LowercaseFilter()
     stem_analyzer = StemmingAnalyzer(r"[\s/,_'-]", gaps=True, stoplist=None)

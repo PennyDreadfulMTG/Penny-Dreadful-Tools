dbcfb87cd01ca396ca5bf4efe04b52b9049fb58e
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-04-23 15:18:12 +1000

    Improve scrapers with vcr.

--- decksite/scrapers/scraper_test.py
+++ decksite/scrapers/scraper_test.py
@@ -1,4 +1,5 @@
 import pytest
+import vcr
 
 from decksite.main import APP
 from decksite.scrapers import gatherling, tappedout
@@ -15,12 +16,14 @@ def test_tappedout():
 
 @pytest.mark.functional
 @pytest.mark.gatherling
+@vcr.use_cassette(record_mode='new_episodes')
 def test_gatherling():
     with APP.app_context():
         gatherling.scrape(5)
 
 @pytest.mark.functional
 @pytest.mark.tappedout
+@vcr.use_cassette
 def test_manual_tappedout():
     with APP.app_context():
         tappedout.scrape_url('https://tappedout.net/mtg-decks/60-island/') # Best deck

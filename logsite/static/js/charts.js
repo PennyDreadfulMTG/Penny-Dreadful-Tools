4b361b8c6a3ad85ac491a44833306c54456ffd9d
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-04-24 22:49:04 +1000

    More deduplication, and make sure that hours with 0 games still appear in graph.

--- logsite/static/js/charts.js
+++ logsite/static/js/charts.js
@@ -1,17 +1,21 @@
 /*global PD,Chart, moment */
+var ctx = document.getElementById("pdChart").getContext("2d");
+
 function ts2str(ts) {
     var t = moment.unix(ts),
-        s = t.format("dddd LT z");
+        tz = moment.tz.guess(),
+        s = t.tz(tz).format("dddd LT z");
     return s;
 }
 
-function build(ctx, data, label) {
+function build() {
+    var data = PD.recent.formats.PennyDreadful;
     var myChart = new Chart(ctx, {
         type: "bar",
         data: {
             labels: Object.keys(data).map(ts2str),
             datasets: [{
-                label,
+                label: "# Penny Dreadful Games",
                 data: Object.values(data),
                 borderWidth: 1
             }]
@@ -28,10 +32,10 @@ function build(ctx, data, label) {
     });
 }
 
+
 function makeChart(data) {
     PD.recent = data;
-    var ctx = document.getElementById("myChart").getContext("2d");
-    build(ctx, PD.recent.formats.PennyDreadful, "# Penny Dreadful Games");
+    build();
 }
 
 $.get("/recent.json", makeChart);
---
+++ shared_web/api.py
@@ -0,0 +1,43 @@
+import json
+import subprocess
+
+from flask import request, current_app, Response
+
+from shared import configuration
+from shared.serialization import extra_serializer
+
+
+def process_github_webhook():
+    if request.headers.get('X-GitHub-Event') == 'push':
+        payload = json.loads(request.data)
+        expected = 'refs/heads/{0}'.format(current_app.config['branch'])
+        if payload['ref'] == expected:
+            try:
+                subprocess.check_output(['git', 'fetch'])
+                subprocess.check_output(['git', 'reset', '--hard', 'origin/{0}'.format(current_app.config['branch'])])
+                subprocess.check_output(['pip', 'install', '-U', '--user', '-r', 'requirements.txt', '--no-cache'])
+                import uwsgi
+                uwsgi.reload()
+                return return_json({'rebooting': True})
+            except ImportError:
+                pass
+    return return_json({
+        'rebooting': False,
+        'commit-id': current_app.config['commit-id'],
+        'current_branch': current_app.config['branch'],
+        'ref': payload['ref'],
+        'expected': expected
+        })
+
+def validate_api_key():
+    if request.form.get('api_token', None) == configuration.get('pdbot_api_token'):
+        return None
+    return return_json(generate_error('UNAUTHORIZED', 'Invalid API key'), status=403)
+
+def generate_error(code, msg):
+    return {'error': True, 'code': code, 'msg': msg}
+
+def return_json(content, status=200):
+    content = json.dumps(content, default=extra_serializer)
+    r = Response(response=content, status=status, mimetype='application/json')
+    return r

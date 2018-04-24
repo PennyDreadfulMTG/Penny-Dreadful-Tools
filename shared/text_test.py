ddf92774a706a9c37c11f0344fdb5293cf56587c
Thomas David Baker <bakert@gmail.com>
2018-04-25 01:05:45 +0200

    Typing for serialization and text_test.

--- shared/text_test.py
+++ shared/text_test.py
@@ -1,7 +1,7 @@
 from shared import text
 
 
-def test_sanitize():
+def test_sanitize() -> None:
     assert text.sanitize("Lim-DÃ»l's High Guard") == "Lim-Dûl's High Guard"
     assert text.sanitize("Lim-Dûl's High Guard") == "Lim-Dûl's High Guard"
     assert text.sanitize('Kongming, &quot;Sleeping Dragon&quot;') == 'Kongming, "Sleeping Dragon"'

1e67b3d842c436da89864d66ec354369c50248e5
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-04-26 00:50:51 +1000

    typing.

--- shared/text.py
+++ shared/text.py
@@ -1,7 +1,7 @@
 import html
 
 
-def sanitize(s):
+def sanitize(s: str) -> str:
     """Try and corral the given string-ish thing into a unicode string. Expects input from files in arbitrary encodings and with bits of HTML in them. Useful for Lim-Dûl and similar."""
     try:
         s = s.encode('latin-1').decode('utf-8')

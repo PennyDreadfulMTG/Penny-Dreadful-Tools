e11c396ea23effb25605b13ee1e76c8be0e15276
Thomas David Baker <bakert@gmail.com>
2018-05-05 08:26:26 +0100

    Pointless comments in order to pass linting without combining logsite+decksite.
    See #4743 for my preferred plan.

--- logsite/auth.py
+++ logsite/auth.py
@@ -11,19 +11,25 @@ from . import APP
 def authenticate():
     target = request.args.get('target')
     authorization_url, state = oauth.setup_authentication()
+    # logsite duplication, see #4743
     session['oauth2_state'] = state
     if target is not None:
         session['target'] = target
+    # logsite duplication, see #4743
     return redirect(authorization_url)
 
 @APP.route('/authenticate/callback/')
 def authenticate_callback():
+    # logsite duplication, see #4743
     if request.values.get('error'):
         return redirect(url_for('unauthorized', error=request.values['error']))
+    # logsite duplication, see #4743
     oauth.setup_session(request.url)
     url = session.get('target')
+    # logsite duplication, see #4743
     if url is None:
         url = url_for('home')
+    # logsite duplication, see #4743
     session['target'] = None
     return redirect(url)
 
@@ -31,6 +37,7 @@ def authenticate_callback():
 def logout():
     oauth.logout()
     target = request.args.get('target', 'home')
+    # logsite duplication, see #4743
     if bool(urllib.parse.urlparse(target).netloc):
         return redirect(target)
     return redirect(url_for(target))

e0d534c5aa6207833fb1787aaf13076ed65af44d
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-05-10 14:58:22 +1000

    Keep season table up to date.

---
+++ decksite/maintenance/insert_seasons.py
@@ -0,0 +1,12 @@
+from ..database import db
+from magic import rotation
+from shared import dtutil
+
+def run():
+    existing = list(map(lambda s: s['number'], db().execute('SELECT `number` FROM `season`;')))
+    for season, setcode in enumerate(rotation.SEASONS, start=1):
+        if not season in existing:
+            info = rotation.get_set_info(setcode)
+            if info['enter_date_dt'] < dtutil.now():
+                print('Inserting {} into season table.'.format(setcode))
+                db().execute('INSERT INTO season (`number`, code, start_date) VALUES (%s, %s, %s);', [season, setcode, dtutil.dt2ts(info['enter_date_dt'])])

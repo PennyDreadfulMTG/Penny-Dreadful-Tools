53bf638b07cc693f8ef0f17302a628c0d5ed21dc
Thomas David Baker <bakert@gmail.com>
2018-05-16 13:36:00 +0100

    Typing.

--- decksite/data/deck_test.py
+++ decksite/data/deck_test.py
@@ -1,8 +1,10 @@
+from typing import List
+
 from decksite.data import deck
 
 
 def test_load_similar_decks() -> None:
-    ds = []
+    ds: List[deck.Deck] = []
     deck.load_similar_decks(ds)
     assert ds == []
     d = deck.Deck({'id': 0})

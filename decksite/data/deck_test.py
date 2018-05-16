882a3abadeb8f3a8e4c70a4684984dbbe440ca79
Thomas David Baker <bakert@gmail.com>
2018-05-16 13:33:39 +0100

    Remove unnecessary parens.

--- decksite/data/deck_test.py
+++ decksite/data/deck_test.py
@@ -3,9 +3,9 @@ from decksite.data import deck
 def test_load_similar_decks() -> None:
     ds = []
     deck.load_similar_decks(ds)
-    assert(ds == [])
+    assert ds == []
     d = deck.Deck({'id': 0})
     d.maindeck = [{'n': 60, 'name': 'Ancestral Recall'}]
     d.sideboard = []
     deck.load_similar_decks([d])
-    assert(d.similar_decks == [])
+    assert d.similar_decks == []

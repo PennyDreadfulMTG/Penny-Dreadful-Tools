31abe89fd6ab837e3b28f1152bfa535268346d8f
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-05-17 11:59:06 +1000

    Fancy test to ensure that all {variables} are shown in translations.

--- maintenance/update_translations.py
+++ maintenance/update_translations.py
@@ -5,7 +5,7 @@ from poeditor.client import POEditorAPI
 
 from shared import configuration
 from shared_web import logger
-
+from . import validate_translations
 
 def run():
     api_key = configuration.get('poeditor_api_key')
@@ -18,17 +18,22 @@ def run():
     for locale in languages:
         logger.warning('Found translation for {code}: {percent}%'.format(code=locale['code'], percent=locale['percentage']))
         if locale['percentage'] > 0:
-            path = os.path.join('decksite', 'translations', locale['code'].replace('-', '_'), 'LC_MESSAGES')
+            path = os.path.join('shared_web', 'translations', locale['code'].replace('-', '_'), 'LC_MESSAGES')
             if not os.path.exists(path):
                 os.makedirs(path)
             pofile = os.path.join(path, 'messages.po')
             logger.warning('Saving to {0}'.format(pofile))
             if os.path.exists(pofile):
                 os.remove(pofile)
-            client.export('162959', locale['code'], local_file=pofile)
+            client.export(project_id='162959', language_code=locale['code'],
+                          local_file=pofile, filters='not_fuzzy')
+                          # Filers should be ['translated', 'not_fuzzy'].
+                          # Blocked by https://github.com/sporteasy/python-poeditor/pull/10
+
     # Compile .po files into .mo files
+    validate_translations.ad_hoc()
     compiler = compile_catalog()
-    compiler.directory = os.path.join('decksite', 'translations')
+    compiler.directory = os.path.join('shared_web', 'translations')
     compiler.domain = ['messages']
     compiler.run()
     # hack for English - We need an empty folder so that Enlish shows up in the 'Known Languages' list.
---
+++ maintenance/validate_translations.py
@@ -0,0 +1,56 @@
+from typing import Optional
+import re
+import os
+from babel.messages import pofile
+from babel.messages.catalog import Catalog, Message
+
+def ad_hoc():
+    for directory, _, files in os.walk(os.path.join('shared_web', 'translations')):
+        for path in [os.path.join(directory, f) for f in files if os.path.splitext(f)[1] == '.po']:
+            print(path)
+            validate_pofile(path)
+
+def validate_pofile(path: str) -> None:
+    with open(path, mode='rb+') as f:
+        catalog = pofile.read_po(f)
+        messages = [message for message in catalog]
+        for message in messages:
+            if message.id and message.string:
+                validate_string(message, catalog)
+        f.seek(0)
+        f.truncate()
+        pofile.write_po(f, catalog)
+
+def validate_string(message: Message, catalog: Catalog) -> None:
+    if isinstance(message.string, str):
+        warning = has_missing_var(message.id, message.string)
+        if warning:
+            error(message, catalog, warning)
+    else:
+        for x in range(len(message.string)):
+            s = message.string[x]
+            try:
+                warning = has_missing_var(message.id[x], s)
+            except IndexError:
+                # The russian translation has multiple variations of plural
+                warning = has_missing_var(message.id[-1], s)
+            if warning:
+                error(message, catalog, warning)
+                return
+
+
+def has_missing_var(english: str, string: str) -> Optional[str]:
+    try:
+        for m in re.findall(r'\{\w+\}', english):
+            if not m in string:
+                return 'Variable {m} missing from translation'.format(m=m)
+        return None
+    except TypeError:
+        print(repr(english))
+        print(type(english))
+        raise
+
+def error(message: Message, catalog: Catalog, warning: str) -> None:
+    print('Warning: {lang} {message} failed tests:\n... {str}\n... {warning}'.format(message=message.id, lang=catalog.locale, warning=warning, str=message.string))
+    # catalog.delete(message.id)
+    catalog.add(message.id, message.string, flags=['fuzzy'])

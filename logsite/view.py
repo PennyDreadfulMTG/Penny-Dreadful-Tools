PennyDreadfulMTG/Penny-Dreadful-Tools

commit d51c137d79c723ecd2e0d408fd052aca8f070837

Use last git revision of file not HEAD for static files cache-bust.
This way it updates only when the file updates and Expires header will stop
them being downloaded so often.

Fixes #1963.

shared_web/base_view.py
@@ -1,3 +1,4 @@
+import subprocess
 from typing import Dict, List, Optional, Union
 
 from flask import current_app, url_for
@@ -22,14 +23,17 @@ class BaseView:
     def prepare(self) -> None:
         return
 
-    def commit_id(self) -> str:
-        return current_app.config['commit-id']
+    def commit_id(self, path: str = None) -> str:
+        args = ['git', 'log', '--format="%H"', '-n', '1']
+        if path:
+            args.append(path)
+        return subprocess.check_output(args, universal_newlines=True).strip('\n').strip('"')
 
     def git_branch(self) -> str:
         return current_app.config['branch']
 
     def css_url(self) -> str:
-        return current_app.config['css_url'] or url_for('static', filename='css/pd.css', v=self.commit_id())
+        return current_app.config['css_url'] or url_for('static', filename='css/pd.css', v=self.commit_id('decksite/static/css/pd.css'))
 
     def tooltips_url(self) -> Optional[str]:
         # Don't preload 10,000 images.
@@ -39,7 +43,7 @@ class BaseView:
         return url_for('static', filename='js/tooltips.js', v=self.commit_id())
 
     def js_url(self) -> str:
-        return current_app.config['js_url'] or url_for('static', filename='js/pd.js', v=self.commit_id())
+        return current_app.config['js_url'] or url_for('static', filename='js/pd.js', v=self.commit_id('decksite/static/js/pd.js'))
 
     def language_icon(self):
         return url_for('static', filename='images/language_icon.svg')

commit a1e864f29d60bd975382790274d3b52e2511a334

Fix regression introduced in d51c137d79c723ecd2e0d408fd052aca8f070837.
The fix for #1963 involved running three seperate `git` executions per page request.
This commit fixes that by once again caching commit-ids on the flask application instance.

logsite/view.py
@@ -2,9 +2,6 @@ from flask import url_for
 
 from shared_web.base_view import BaseView
 
-from . import APP
-
-
 # pylint: disable=no-self-use, too-many-public-methods
 class View(BaseView):
     def js_extra_url(self):
@@ -23,6 +20,3 @@ class View(BaseView):
 
     def subtitle(self):
         return None
-
-    def commit_id(self, path: str = None) -> str:
-        return APP.config['commit-id']

shared_web/base_view.py
@@ -24,10 +24,15 @@ class BaseView:
         return
 
     def commit_id(self, path: str = None) -> str:
-        args = ['git', 'log', '--format="%H"', '-n', '1']
-        if path:
-            args.append(path)
-        return subprocess.check_output(args, universal_newlines=True).strip('\n').strip('"')
+        if not path:
+            return current_app.config['commit-id']
+        key = f'commit-id-{path}'
+        commit = current_app.config.get(key, None)
+        if commit is None:
+            args = ['git', 'log', '--format="%H"', '-n', '1', path]
+            commit = subprocess.check_output(args, universal_newlines=True).strip('\n').strip('"')
+            current_app.config[key] = commit
+        return commit
 
     def git_branch(self) -> str:
         return current_app.config['branch']
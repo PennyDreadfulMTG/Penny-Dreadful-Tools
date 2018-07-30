06cc67d833082a32e32ae0cbf256c809b3710a9a
Thomas David Baker <bakert@gmail.com>
2018-07-30 12:09:49 -0700

    Hide intro box for signed in folks.
    People don't realize they can dismiss it and leave it around forever.
    
    If you've got around to signing in you know the basics or can find them on the
    FAQ page.

--- decksite/api.py
+++ decksite/api.py
@@ -130,7 +130,7 @@ def person_status():
         'mtgo_username': auth.mtgo_username(),
         'discord_id': auth.discord_id(),
         'admin': session.get('admin', False),
-        'hide_intro': request.cookies.get('hide_intro', False) or auth.hide_intro(),
+        'hide_intro': request.cookies.get('hide_intro', False) or auth.hide_intro() or auth.mtgo_username() or auth.discord_id(),
         'in_guild': session.get('in_guild', False),
         }
     if auth.mtgo_username():

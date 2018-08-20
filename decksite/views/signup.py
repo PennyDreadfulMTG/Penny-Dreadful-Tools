26511de61295de96d4b1dbe330ee271b6e49e34c
Thomas David Baker <bakert@gmail.com>
2018-08-20 14:21:59 +0200

    Reduce duplication by creating a parent class for deckcheck and signup.
    View
    |
    |-> LeagueForm
        |
        |-> Report
        |-> Retire
        |-> DecklistForm
            |
            |-> SignUp
            |-> DeckCheck
    
    is the new hierarchy.

--- decksite/views/signup.py
+++ decksite/views/signup.py
@@ -1,22 +1,10 @@
 from flask_babel import gettext
 
-from decksite.views.league_form import LeagueForm
+from decksite.views.decklist_form import DecklistForm
 
 
 # pylint: disable=no-self-use
-class SignUp(LeagueForm):
-    def __init__(self, form, person_id: int = None) -> None:
-        super().__init__(form)
-        self.person_id = person_id
-        self.classify_illegal_cards()
-
-    def classify_illegal_cards(self) -> None:
-        if self.form.card_errors is not None:
-            self.has_not_legal = 'Legality_Not_Legal' in self.form.card_errors and len(self.form.card_errors['Legality_Not_Legal']) > 0
-            self.has_banned = 'Legality_Banned' in self.form.card_errors and len(self.form.card_errors['Legality_Banned']) > 0
-            self.has_bugs = 'Legality_Bugs' in self.form.card_errors and len(self.form.card_errors['Legality_Bugs']) > 0
-            self.has_too_many = 'Legality_Too_Many' in self.form.card_errors and len(self.form.card_errors['Legality_Too_Many']) > 0
-
+class SignUp(DecklistForm):
     def page_title(self):
         return '{league} Sign Up'.format(league=self.league['name'])
 

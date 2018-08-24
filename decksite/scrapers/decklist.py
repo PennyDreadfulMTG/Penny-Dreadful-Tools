3791fc401faecdc1cb7a123b1de57f6631a0c2f2
Thomas David Baker <bakert@gmail.com>
2018-08-24 15:45:57 +0200

    Use correct return type.

--- decksite/scrapers/decklist.py
+++ decksite/scrapers/decklist.py
@@ -62,7 +62,7 @@ def parse(s: str) -> DecklistType:
 
     return {'maindeck':maindeck, 'sideboard':sideboard}
 
-def looks_doublespaced(s: str) -> str:
+def looks_doublespaced(s: str) -> bool:
     return len(re.findall(r'\r?\n\r?\n', s)) >= len(re.findall(r'\r?\n\r?\n')) / 2 - 1
 
 def remove_doublespacing(s: str) -> str:

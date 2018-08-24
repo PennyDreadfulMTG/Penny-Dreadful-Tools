92e0fbd129755d612a9b8fc20bc97ce1a8fd36aa
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-08-24 16:43:06 +1000

    Reprocess issues when they're updated.

--- modo_bugs/webhooks.py
+++ modo_bugs/webhooks.py
@@ -1,6 +1,8 @@
 from github_webhook import Webhook
 
+from shared import configuration
 from shared_web.flask_app import PDFlask
+from . import update, repo
 
 APP = PDFlask(__name__)
 WEBHOOK = Webhook(APP, endpoint='/api/github')
@@ -9,6 +11,15 @@ WEBHOOK = Webhook(APP, endpoint='/api/github')
 def home():
     return 'build-commit-id: ' + APP.config['commit-id']
 
+def get_number(url: str) -> int:
+    split_url = url.split('/')
+    return int(split_url[7])
+
+
 @WEBHOOK.hook(event_type='issues')
 def on_issues(data):
-    pass
+    if data['sender']['login'] == configuration.get_str('github_user'):
+        return
+    number = get_number(data['pull_request']['url'])
+    issue = repo.get_repo().get_issue(number)
+    update.process_issue(issue)

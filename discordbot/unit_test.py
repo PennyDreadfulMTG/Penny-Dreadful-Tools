715c08a4275c08fc46bbef6acee9fb3bbb946d25
Thomas David Baker <bakert@gmail.com>
2018-09-01 16:18:18 +0100

    Layout is now required in dummy card because it is used in image downloading.

--- discordbot/unit_test.py
+++ discordbot/unit_test.py
@@ -25,7 +25,7 @@ def test_fallbackimagedownload() -> None:
 
 # Check that we can succesfully fail at getting an image.
 def test_noimageavailable() -> None:
-    c = Card({'name': "Barry's Land", 'id': 0, 'multiverseid': 0, 'names': "Barry's Land"})
+    c = Card({'name': "Barry's Land", 'id': 0, 'multiverseid': 0, 'names': "Barry's Land", 'layout': 'normal'})
     assert image_fetcher.download_image([c]) is None
 
 # Search for a single card via full name,

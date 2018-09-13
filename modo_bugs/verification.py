a321d656e50b5c24f98a5fb1a767d9f5e4b3b275
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-09-14 00:24:07 +1000

    Progression towards the ideal Verification project capabilities.
    We need additional features from pygithub before we go any further.

--- modo_bugs/verification.py
+++ modo_bugs/verification.py
@@ -1,6 +1,7 @@
 import requests
 from lxml import etree
 
+from . import repo
 
 def main() -> None:
     manifest = requests.get('http://mtgoclientdepot.onlinegaming.wizards.com/MTGO.application')
@@ -9,3 +10,8 @@ def main() -> None:
     version = identity.attrib['version']
 
     print('Current MTGO Version is {0}'.format(version))
+
+    project = repo.get_verification_project()
+    current = [c for c in project.get_columns() if c.name == version]
+    if not current:
+        print(f'We need a column for {version}')

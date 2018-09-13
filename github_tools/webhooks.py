48113f49282d9225ed983b8178fa9eb9ea0ce6a8
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-09-13 23:08:21 +1000

    Better handling of unexpected requirements.txt
    Fixes #5080

--- github_tools/webhooks.py
+++ github_tools/webhooks.py
@@ -83,15 +83,15 @@ def build_changelog(change: File) -> List[str]:
     oldversions: Dict[str, str] = {}
     newversions: Dict[str, str] = {}
     for p in patch.changes:
-        try:
+        if len(p) > 1:
             if p[1] is None:
                 req = Requirement.parse(p[2])
                 oldversions[req.name] = req.specs[0][1]
             elif p[0] is None:
                 req = Requirement.parse(p[2])
                 newversions[req.name] = req.specs[0][1]
-        except (IndexError, TypeError) as e:
-            print(f'Patch change does not match structure we expect. Got {e} looking at {p}.')
+        else:
+            lines.append(f'> Warning: {p[0]} is pinned to a specific version.')
     for package in newversions:
         old = packaging.version.parse(oldversions.get(package))
         new = packaging.version.parse(newversions.get(package))

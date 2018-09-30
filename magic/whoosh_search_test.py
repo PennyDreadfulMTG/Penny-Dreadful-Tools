7568fba491904a4f9b5d668e519769e7f2dcf108
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-10-01 00:25:54 +1000

    Make the xfail strict, so we remember to remove it when mtgjson fixes things.

--- magic/whoosh_search_test.py
+++ magic/whoosh_search_test.py
@@ -76,7 +76,7 @@ class WhooshSearchTest(unittest.TestCase):
     def test_normalized_beats_tokenized(self) -> None:
         self.best_match_is('Flash Food', 'Flash Flood')
 
-    @pytest.mark.xfail(reason='There is a bug with the current version of mtgjson')
+    @pytest.mark.xfail(reason='There is a bug with the current version of mtgjson', strict=True)
     def test_10_cycles_are_returned(self) -> None:
         result = self.searcher.search('Guildgate') # type: ignore
         assert len(result.fuzzy) == 10

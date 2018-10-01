12ab78d411ede36a33273a2736602de29feaa1fb
Thomas David Baker <bakert@gmail.com>
2018-10-01 13:50:16 -0700

    Allow bootstrapping.

--- decksite/sql/42.sql
+++ decksite/sql/42.sql
@@ -1,4 +1,4 @@
-DROP TABLE _archetype_stats;
-DROP TABLE _matchup_stats;
-DROP TABLE _card_stats;
-DROP TABLE _card_person_stats;
+DROP TABLE IF EXISTS _archetype_stats;
+DROP TABLE IF EXISTS _matchup_stats;
+DROP TABLE IF EXISTS _card_stats;
+DROP TABLE IF EXISTS _card_person_stats;

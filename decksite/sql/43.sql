88281ed955062f414fcaaecc0c828f76a73de343
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2018-10-21 09:14:08 +0100

    rename guidance to description

--- decksite/sql/43.sql
+++ decksite/sql/43.sql
@@ -1,7 +1,7 @@
-ALTER TABLE archetype ADD COLUMN guidance TEXT;
-UPDATE archetype SET guidance='';
-UPDATE archetype SET guidance='Kill the opponent quickly by attacking with creatures.' WHERE name='Aggro';
-UPDATE archetype SET guidance='Execute a specific interaction which immediately wins the game or generates insurmountable advantage.' WHERE name='Combo';
-UPDATE archetype SET guidance='Answer threats while generating card advantage.' WHERE name='Control';
-UPDATE archetype SET guidance='Accelerate mana to play big threats before they can be answered.' WHERE name='Ramp';
-UPDATE archetype SET guidance='Do not add decks to this archetype' WHERE name='Unclassified';
+ALTER TABLE archetype ADD COLUMN description TEXT;
+UPDATE archetype SET description='';
+UPDATE archetype SET description='Kill the opponent quickly by attacking with creatures.' WHERE name='Aggro';
+UPDATE archetype SET description='Execute a specific interaction which immediately wins the game or generates insurmountable advantage.' WHERE name='Combo';
+UPDATE archetype SET description='Answer threats while generating card advantage.' WHERE name='Control';
+UPDATE archetype SET description='Accelerate mana to play big threats before they can be answered.' WHERE name='Ramp';
+UPDATE archetype SET description='Do not add decks to this archetype' WHERE name='Unclassified';

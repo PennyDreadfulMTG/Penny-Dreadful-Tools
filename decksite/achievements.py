df29fb12ae2359d2c732f04de767cbab10e939bc
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2018-10-23 09:26:13 +0100

    be less parsimonious with characters

--- decksite/achievements.py
+++ decksite/achievements.py
@@ -1,6 +1,5 @@
-from typing import TYPE_CHECKING, Dict, List, Optional
-
 import re
+from typing import TYPE_CHECKING, Dict, List, Optional
 
 from flask import url_for
 from flask_babel import ngettext
@@ -15,7 +14,7 @@ if TYPE_CHECKING:
 
 def load_query(people_by_id: Dict[int, 'person.Person'], season_id: Optional[int]) -> str:
     # keys have been normalised earlier but could still be reserved words
-    columns = ', '.join(f'SUM({a.key}) as `{a.key}`' for a in Achievement.all_achs if a.in_db)
+    columns = ', '.join(f'SUM({a.key}) as `{a.key}`' for a in Achievement.all_achievements if a.in_db)
     return """
         SELECT
             person_id AS id,
@@ -29,8 +28,8 @@ def load_query(people_by_id: Dict[int, 'person.Person'], season_id: Optional[int
     """.format(columns=columns, ids=', '.join(str(k) for k in people_by_id.keys()), season_query=query.season_query(season_id))
 
 def preaggregate_query() -> str:
-    create_columns = ', '.join(f'`{a.key}` INT NOT NULL' for a in Achievement.all_achs if a.in_db)
-    select_columns = ', '.join(f'{a.sql} as `{a.key}`' for a in Achievement.all_achs if a.in_db)
+    create_columns = ', '.join(f'`{a.key}` INT NOT NULL' for a in Achievement.all_achievements if a.in_db)
+    select_columns = ', '.join(f'{a.sql} as `{a.key}`' for a in Achievement.all_achievements if a.in_db)
     return """
         CREATE TABLE IF NOT EXISTS _new_achievements (
             person_id INT NOT NULL,
@@ -60,15 +59,15 @@ def preaggregate_query() -> str:
     """.format(cc=create_columns, sc=select_columns, season_join=query.season_join(), competition_join=query.competition_join())
 
 def descriptions() -> List[Dict[str, str]]:
-    return [{'title': a.title, 'description_safe': a.description_safe} for a in Achievement.all_achs]
+    return [{'title': a.title, 'description_safe': a.description_safe} for a in Achievement.all_achievements]
 
 def displayed_achievements(p: 'person.Person') -> List[Dict[str, str]]:
-    return [d for d in (a.display(p) for a in Achievement.all_achs) if d is not None]
+    return [d for d in (a.display(p) for a in Achievement.all_achievements) if d is not None]
 
 # Abstract achievement classes
 
 class Achievement:
-    all_achs: List['Achievement'] = []
+    all_achievements: List['Achievement'] = []
     key: Optional[str] = None
     sql: Optional[str] = None
     in_db = True
@@ -78,9 +77,9 @@ class Achievement:
         if cls.key is not None:
             # in case anyone ever makes a poor sportsmanship achievement called DROP TABLE
             cls.key = re.sub('[^A-Za-z0-9_]+', '', cls.key)
-            if cls.key in [c.key for c in cls.all_achs]:
+            if cls.key in [c.key for c in cls.all_achievements]:
                 print(f"Warning: Two achievements have the same normalised key {cls.key}. This won't do any permanent damage to the database but the results are almost certainly not as intended.")
-            cls.all_achs.append(cls())
+            cls.all_achievements.append(cls())
     @staticmethod
     def display(_: 'person.Person') -> Optional[Dict[str, str]]:
         return None

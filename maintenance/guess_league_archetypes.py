9070121c5b2c4278afb47947a59643f2c4cbe2aa
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-10-25 15:42:32 +1100

    Super simple guesser

---
+++ maintenance/guess_league_archetypes.py
@@ -0,0 +1,9 @@
+from decksite.data import archetype, deck
+
+
+def run() -> None:
+    decks = deck.load_decks('archetype_id is null')
+    deck.calculate_similar_decks(decks)
+    for d in decks:
+        if d.similar_decks:
+            archetype.assign(d.id, d.similar_decks[0].archetype_id, False)

8db1e7829961538a34c5cf263b280e65bed527dd
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2018-10-28 09:17:29 +0000

    work out confidence for league deck archetype predictions (but don't do anything with it yet)

--- decksite/data/archetype.py
+++ decksite/data/archetype.py
@@ -121,9 +121,14 @@ def add(name: str, parent: int) -> None:
     sql += '({ancestor}, {descendant}, {depth})'.format(ancestor=archetype_id, descendant=archetype_id, depth=0)
     db().execute(sql)
 
-def assign(deck_id: int, archetype_id: int, reviewed: bool = True) -> None:
+def assign(deck_id: int, archetype_id: int, reviewed: bool = True, confidence: Optional[int] = None) -> None:
     and_clause = '' if reviewed else 'AND reviewed is FALSE'
     db().execute(f'UPDATE deck SET reviewed = %s, archetype_id = %s WHERE id = %s {and_clause}', [reviewed, archetype_id, deck_id])
+    if not reviewed and confidence is not None:
+        db().execute(f'INSERT INTO confidence VALUES (%s, %s) ON DUPLICATE KEY UPDATE score=%s', [deck_id, confidence, confidence])
+
+def clear_old_predictions() -> None:
+    db().execute('DELETE confidence FROM confidence LEFT JOIN deck ON confidence.deck_id = deck.id WHERE deck.reviewed = TRUE')
 
 def load_all_matchups(where: str = 'TRUE', season_id: Optional[int] = None, retry: bool = False) -> List[Container]:
     sql = """
--- maintenance/guess_league_archetypes.py
+++ maintenance/guess_league_archetypes.py
@@ -3,8 +3,10 @@ from decksite.data import archetype, deck
 HOURLY = True
 
 def run() -> None:
+    archetype.clear_old_predictions()
     decks = deck.load_decks('archetype_id is null')
     deck.calculate_similar_decks(decks)
     for d in decks:
         if d.similar_decks:
-            archetype.assign(d.id, d.similar_decks[0].archetype_id, False)
+            conf = int(100 * deck.similarity_score(d, d.similar_decks[0]))
+            archetype.assign(d.id, d.similar_decks[0].archetype_id, False, conf)

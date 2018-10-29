d2e8e4b85987840639efea25d5f7d22b6e1c0423
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-10-29 16:11:32 +1100

    Linter to ensure that people are using gettext in a way that actually works

--- pylint_monolith/__init__.py
+++ pylint_monolith/__init__.py
@@ -1,5 +1,5 @@
 from .monolith_checker import MonolithChecker
-
+from .l18n_check import TranslationStringConstantsChecker
 
 def register(linter):
     """Required method to auto register this checker.
@@ -8,3 +8,4 @@ def register(linter):
         linter: Main interface object for Pylint plugins.
     """
     linter.register_checker(MonolithChecker(linter))
+    linter.register_checker(TranslationStringConstantsChecker(linter))

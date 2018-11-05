9c2efb5fec53f63359527c295e6437df4bd163be
Thomas David Baker <bakert@gmail.com>
2018-11-05 17:32:50 -0500

    Home page is no longer guaranteed to have this text on it.
    It will not when there are no decks in the last 30 days (true for Travis build).

--- decksite/smoke_test.py
+++ decksite/smoke_test.py
@@ -11,10 +11,6 @@ class DecksiteSmokeTest(unittest.TestCase):
         self.tester: SmokeTester = SmokeTester(APP)
 
     @pytest.mark.functional
-    def test_home(self) -> None:
-        self.tester.data_test('/', '<h1><string>Latest Decks</string></h1>')
-
-    @pytest.mark.functional
     def test_some_pages(self) -> None:
         for path in ['/', '/people/', '/cards/', '/cards/Unsummon/', '/competitions/', '/competitions/', '/tournaments/', '/resources/', '/bugs/', '/signup/', '/report/']:
             self.tester.response_test(path, 200)

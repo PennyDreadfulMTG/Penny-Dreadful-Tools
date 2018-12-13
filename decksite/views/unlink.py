382e8e93fb7b18036b733d13dfc33c2106489011
Thomas David Baker <bakert@gmail.com>
2018-12-13 10:23:31 -0800

    Don't error out if someone puts an mtgo username into Discord ID box.
    Fixes #5881.

--- decksite/views/unlink.py
+++ decksite/views/unlink.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, List
 
 from decksite.data.person import Person
 from decksite.view import View
@@ -6,11 +6,12 @@ from decksite.view import View
 
 # pylint: disable=no-self-use
 class Unlink(View):
-    def __init__(self, people: Iterable[Person], num_affected_people: int = None) -> None:
+    def __init__(self, people: Iterable[Person], num_affected_people: int = None, errors: List[str] = None) -> None:
         super().__init__()
         self.people = people
         if num_affected_people is not None:
             self.message = f'{num_affected_people} were affected'
+        self.errors = errors or []
 
     def page_title(self):
         return 'Unlink'

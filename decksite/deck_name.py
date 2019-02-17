7f7804c3e083ec4c386c19d9598283fd80b6dacd
Thomas David Baker <bakert@gmail.com>
2019-02-16 20:32:38 -0800

    More deck sanitization.
    Fixes #6058.

--- decksite/deck_name.py
+++ decksite/deck_name.py
@@ -100,7 +100,7 @@ def replace_space_alternatives(name: str) -> str:
     return name.replace('_', ' ').replace('.', ' ')
 
 def remove_pd(name: str) -> str:
-    name = re.sub(r'(^| )[\[\(]?pd-?[0-9]+[\[\(]?', '', name, flags=re.IGNORECASE).strip()
+    name = re.sub(r'(^| )[\[\(]?pd ?-? ?[0-9]+[\[\(]?', '', name, flags=re.IGNORECASE).strip()
     name = re.sub(r'(^| )[\[\(]?pd[hmstf]?[\]\)]?([ -]|$)', '', name, flags=re.IGNORECASE).strip()
     name = re.sub(r'(^| )[\[\(]?penny ?dreadful (sunday|monday|thursday)[\[\(]?( |$)', '', name, flags=re.IGNORECASE).strip()
     name = re.sub(r'(^| )[\[\(]?penny ?dreadful[\[\(]?( |$)', '', name, flags=re.IGNORECASE).strip()

8d6dba7d76ba24f4406bfbce7f43f07d445ecbf0
Thomas David Baker <bakert@gmail.com>
2019-03-23 10:44:51 -0700

    Lowercase mtgo usernames in /matchups/.

--- decksite/views/matchups.py
+++ decksite/views/matchups.py
@@ -21,7 +21,7 @@ class Matchups(View):
         # Set up options for dropdowns, marking the right ones as selected.
         for c in self.criteria:
             c['archetypes'] = [{'name': a.name, 'id': a.id, 'selected': str(c['choices'].get('archetype_id')) == str(a.id)} for a in archetypes] # type: ignore
-            c['people'] = [{'mtgo_username': p.mtgo_username, 'id': p.id, 'selected': str(c['choices'].get('person_id')) == str(p.id)} for p in people] # type: ignore
+            c['people'] = [{'mtgo_username': p.mtgo_username.lower(), 'id': p.id, 'selected': str(c['choices'].get('person_id')) == str(p.id)} for p in people] # type: ignore
             c['cards'] = [{'name': card.name, 'selected': c['choices'].get('card') == card.name} for card in cards] # type: ignore
         self.seasons = [{'season_id': s['num'] or '', 'name': s['name'], 'selected': str(season_id) == str(s['num'])} for s in self.all_seasons()]
         self.decks = results.get('hero_decks', []) # type: ignore

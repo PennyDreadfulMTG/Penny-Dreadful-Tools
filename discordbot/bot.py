46999869575875a532cb02246d81801de50e681f
Katelyn Gigante <clockwork.singularity@gmail.com>
2019-10-25 20:33:18 +1100

    Only hide hype in the first few days.

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -350,7 +350,7 @@ def rotation_hype_message() -> Optional[str]:
     num_legal_cards = len([c for c in cs if c.status == 'Legal'])
     name = 'Supplemental rotation' if rotation.next_rotation_is_supplemental() else 'Rotation'
     s = f'{name} run number {runs} completed. {name} is {runs_percent}% complete. {num_legal_cards} cards confirmed.'
-    if newly_hit == 0 and newly_legal == 0 and newly_eliminated == 0 and runs not in [1, rotation.TOTAL_RUNS] and runs % 5 != 0:
+    if newly_hit == 0 and newly_legal == 0 and newly_eliminated == 0 and runs != 1 and runs % 5 != 0 and runs < rotation.TOTAL_RUNS / 2:
         return None # Sometimes there's nothing to report
     if len(newly_hit) > 0 and runs_remaining > runs:
         newly_hit_s = list_of_most_interesting(newly_hit)

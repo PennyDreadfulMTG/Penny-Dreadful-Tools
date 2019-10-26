ebeea066c031cd1f8f5a6a80d653cd2c56d25ac3
Katelyn Gigante <clockwork.singularity@gmail.com>
2019-10-26 15:01:14 +1100

    Someone expected !his to work

--- discordbot/commands/update.py
+++ discordbot/commands/update.py
@@ -7,6 +7,7 @@ from magic import multiverse, oracle
 @commands.command(hidden=True)
 async def update(ctx: MtgContext) -> None:
     """Forces an update to legal cards and bugs."""
+    await ctx.send('Begun reloading legal cards and bugs.')
     multiverse.set_legal_cards()
     oracle.legal_cards(force=True)
     multiverse.update_bugged_cards()

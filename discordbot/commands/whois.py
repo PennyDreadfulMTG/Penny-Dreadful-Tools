610205ac47d9ee764e15e6d4e39262c4d4d57bde
Thomas David Baker <bakert@gmail.com>
2019-10-31 23:55:42 +0000

    Capture more info when #6201 happens.

--- discordbot/commands/whois.py
+++ discordbot/commands/whois.py
@@ -16,6 +16,9 @@ async def whois(ctx: MtgContext, *, args: str) -> None:
         if person is None:
             await ctx.send(f"I don't know who {mention.group(0)} is :frowning:")
             return
+        if person.get('name') is None:
+            await ctx.send(f"I know this person but I don't know their name. That's weird. Here's what I know: {person}")
+            return
         await ctx.send(f"{mention.group(0)} is **{person['name']}** on MTGO")
     else:
         async with ctx.typing():

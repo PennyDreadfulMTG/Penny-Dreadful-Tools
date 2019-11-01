6b887cc9557b1103e59df04959c1800dd404ed56
Thomas David Baker <bakert@gmail.com>
2019-11-01 23:18:43 +0000

    Webpack config for building react deck table.

---
+++ shared_web/static/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const config = {
+    entry:  __dirname + '/js/index.jsx',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          loaders: ['style-loader', 'css-loader'],
+        }
+      ]
+    },
+};
+module.exports = config;

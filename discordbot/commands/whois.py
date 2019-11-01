998f9b45b56067dcadb5eb4dfa7d911b4f735c59
Thomas David Baker <bakert@gmail.com>
2019-11-01 10:02:25 +0000

    Improvements to whois now I understand output from API better.

--- discordbot/commands/whois.py
+++ discordbot/commands/whois.py
@@ -10,23 +10,20 @@ from magic import fetcher
 async def whois(ctx: MtgContext, *, args: str) -> None:
     """Who is a person?"""
     mention = re.match(r'<@!?(\d+)>', args)
-    if mention:
-        async with ctx.typing():
+    async with ctx.typing():
+        if mention:
             person = await fetcher.person_data_async(mention.group(1))
-        if person is None:
-            await ctx.send(f"I don't know who {mention.group(0)} is :frowning:")
-            return
-        if person.get('name') is None:
-            await ctx.send(f"I know this person but I don't know their name. That's weird. Here's what I know: {person}")
-            return
-        await ctx.send(f"{mention.group(0)} is **{person['name']}** on MTGO")
-    else:
-        async with ctx.typing():
+            if not_found(person) or person.get('name') is None:
+                msg = f"I don't know who {mention.group(0)} is :frowning:"
+            else:
+                msg = f"{mention.group(0)} is **{person['name']}** on MTGO"
+        else:
             person = await fetcher.person_data_async(args)
-        if person is None or person.get('discord_id') is None:
-            await ctx.send(f"I don't know who **{args}** is :frowning:")
-            return
-        if person.get('name') is None:
-            await ctx.send(f"I know this person but I don't know their name. That's weird. Here's what I know: {person}")
-            return
-        await ctx.send(f"**{person['name']}** is <@{person['discord_id']}>")
+            if not_found(person) or person.get('discord_id') is None:
+                msg = f"I don't know who **{args}** is :frowning:"
+            else:
+                msg = f"**{person['name']}** is <@{person['discord_id']}>"
+        await ctx.send(msg)
+
+def not_found(person):
+    return person is None or (person.get('error') and person.get('code') == 'NOTFOUND')

7595acf99d6f946930ff9e010dc7bcf32ac5ba89
Thomas David Baker <bakert@gmail.com>
2019-11-03 16:04:56 +0000

    Lint.

--- decksite/webpack.config.js
+++ decksite/webpack.config.js
@@ -1,22 +1,22 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 const config = {
-    entry: __dirname + '/../shared_web/static/js/index.jsx',
+    entry: __dirname + "/../shared_web/static/js/index.jsx",
     output: {
-        path: __dirname + '/../shared_web/static//dist',
-        filename: 'bundle.js',
+        path: __dirname + "/../shared_web/static//dist",
+        filename: "bundle.js",
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.css']
+        extensions: [".js", ".jsx", ".css"]
     },
     module: {
         rules: [{
             test: /\.jsx?/,
             exclude: /node_modules/,
-            use: 'babel-loader'
+            use: "babel-loader"
         }, {
             test: /\.css$/,
             exclude: /node_modules/,
-            loaders: ['style-loader', 'css-loader'],
+            loaders: ["style-loader", "css-loader"],
         }]
     },
 };

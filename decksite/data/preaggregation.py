f362483740f008f73b4b18eeb4f3ceba275882cb
Thomas David Baker <bakert@gmail.com>
2019-11-05 19:32:24 +0000

    Lock around preaggregation - transactions don't work with schema changes.
    Fixes #5328.

--- decksite/data/preaggregation.py
+++ decksite/data/preaggregation.py
@@ -1,9 +1,17 @@
 from decksite.database import db
+from shared.pd_exception import DatabaseException
+from shared_web import logger
 
 def preaggregate(table, sql) -> None:
+    lock_key = f'preaggregation:{table}'
+    try:
+        db().get_lock(lock_key, 60 * 5)
+    except DatabaseException as e:
+        logger.warning(f'Not preaggregating {table} because of {e}')
     db().execute(f'DROP TABLE IF EXISTS _new{table}')
     db().execute(sql)
     db().execute(f'DROP TABLE IF EXISTS _old{table}')
     db().execute(f'CREATE TABLE IF NOT EXISTS {table} (_ INT)') # Prevent error in RENAME TABLE below if bootstrapping.
     db().execute(f'RENAME TABLE {table} TO _old{table}, _new{table} TO {table}')
     db().execute(f'DROP TABLE IF EXISTS _old{table}')
+    db().release_lock(lock_key)

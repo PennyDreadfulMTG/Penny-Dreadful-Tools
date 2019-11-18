c84352dcdfaab925c392598f12f81a450a39129d
Thomas David Baker <bakert@gmail.com>
2019-11-18 10:31:56 +0000

    Add missing return type.

--- decksite/main.py
+++ decksite/main.py
@@ -410,7 +410,7 @@ def rotation_changes() -> str:
 
 @APP.route('/rotation/changes/files/<any(new,out):type>/')
 @SEASONS.route('/rotation/changes/files/<any(new,out):type>/')
-def rotation_changes_files(type: str):
+def rotation_changes_files(type: str) -> Response:
     changes = oracle.pd_rotation_changes(get_season_id())[0 if type == 'new' else 1]
     s = '\n'.join('4 {name}'.format(name=c.name) for c in changes)
     return make_response(s, 200, {'Content-type': 'text/plain; charset=utf-8', 'Content-Disposition': f'attachment; filename={type}.txt'})

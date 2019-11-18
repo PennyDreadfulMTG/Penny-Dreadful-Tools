b467f1538922e28f5a5216983212300862155268
Thomas David Baker <bakert@gmail.com>
2019-11-18 12:07:47 +0000

    Don't redefine a built-in.

--- decksite/main.py
+++ decksite/main.py
@@ -409,12 +409,12 @@ def rotation_changes() -> str:
     view = RotationChanges(*oracle.pd_rotation_changes(get_season_id()), cs.playability(), query=query)
     return view.page()
 
-@APP.route('/rotation/changes/files/<any(new,out):type>/')
-@SEASONS.route('/rotation/changes/files/<any(new,out):type>/')
-def rotation_changes_files(type: str) -> Response:
-    changes = oracle.pd_rotation_changes(get_season_id())[0 if type == 'new' else 1]
+@APP.route('/rotation/changes/files/<any(new,out):changes_type>/')
+@SEASONS.route('/rotation/changes/files/<any(new,out):changes_type>/')
+def rotation_changes_files(changes_type: str) -> Response:
+    changes = oracle.pd_rotation_changes(get_season_id())[0 if changes_type == 'new' else 1]
     s = '\n'.join('4 {name}'.format(name=c.name) for c in changes)
-    return make_response(s, 200, {'Content-type': 'text/plain; charset=utf-8', 'Content-Disposition': f'attachment; filename={type}.txt'})
+    return make_response(s, 200, {'Content-type': 'text/plain; charset=utf-8', 'Content-Disposition': f'attachment; filename={changes_type}.txt'})
 
 @APP.route('/rotation/speculation/')
 def rotation_speculation() -> str:

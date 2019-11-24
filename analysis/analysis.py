f321bc30e8b8b807ac4ca86ea70ce18589a26b6a
Thomas David Baker <bakert@gmail.com>
2019-11-24 11:49:35 +0000

    Rudimentary log analysis basics.
    See #790.

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -28,27 +28,38 @@ def preaggregate_played_cards_by_person():
             FOREIGN KEY (person_id) REFERENCES person (id)  ON UPDATE CASCADE ON DELETE CASCADE
         ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci AS
         SELECT
-            gcp.name,
-            season.id AS season_id,
-            p.id AS person_id,
-            SUM(CASE WHEN dm.games > odm.games THEN 1 ELSE 0 END) AS wins,
-            SUM(CASE WHEN dm.games < odm.games THEN 1 ELSE 0 END) AS losses,
-            SUM(CASE WHEN dm.games = odm.games THEN 1 ELSE 0 END) AS draws
+            name,
+            season_id AS season_id,
+            person_id,
+            SUM(CASE WHEN my_games > your_games THEN 1 ELSE 0 END) AS wins,
+            SUM(CASE WHEN my_games < your_games THEN 1 ELSE 0 END) AS losses,
+            SUM(CASE WHEN my_games = your_games THEN 1 ELSE 0 END) AS draws
         FROM
-            {logsite_database}._game_card_person AS gcp
-        INNER JOIN
-            person AS p ON gcp.mtgo_username = p.mtgo_username
-        INNER JOIN
-            {logsite_database}.game AS g ON g.id = gcp.game_id
-        INNER JOIN
-            `match` AS m ON m.mtgo_id = g.match_id
-        INNER JOIN
-            deck_match AS dm ON dm.match_id = m.id AND dm.deck_id IN (SELECT id FROM deck WHERE person_id = p.id)
-        INNER JOIN
-            deck_match AS odm ON odm.match_id = m.id AND odm.deck_id NOT IN (SELECT id FROM deck WHERE person_id = p.id)
-        INNER JOIN
-            deck AS d ON dm.deck_id = d.id
-        {season_join}
+            (
+                SELECT
+                    gcp.name,
+                    season.id AS season_id,
+                    p.id AS person_id,
+                    MAX(CASE WHEN p.id = d.person_id THEN dm.games ELSE 0 END) AS my_games,
+                    MAX(CASE WHEN p.id <> d.person_id THEN dm.games ELSE 0 END) AS your_games
+                FROM
+                    {logsite_database}._game_card_person AS gcp
+                INNER JOIN
+                    person AS p ON gcp.mtgo_username = p.mtgo_username
+                INNER JOIN
+                    {logsite_database}.game AS g ON g.id = gcp.game_id
+                INNER JOIN
+                    `match` AS m ON m.mtgo_id = g.match_id
+                INNER JOIN
+                    deck_match AS dm ON dm.match_id = m.id
+                INNER JOIN
+                    deck AS d ON dm.deck_id = d.id
+                {season_join}
+                GROUP BY
+                    name, p.id, m.id
+            ) AS base
+        GROUP BY
+            name, person_id, season_id
     """.format(table=table, logsite_database=configuration.get('logsite_database'), season_join=query.season_join())
     print(sql)
     preaggregation.preaggregate(table, sql)
@@ -73,8 +84,18 @@ def played_cards_by_person(person_id: int, season_id: int) -> List[Card]:
 
 def process_logs() -> None:
     init()
-    sql = 'SELECT id AS game_id, log FROM game WHERE id NOT IN (SELECT game_id FROM _game_card_person) LIMIT 1000'
-    rs = logsite_db().select(sql)
+    ids_to_process = next_ids()
+    placeholders = ', '.join('%s' for v in ids_to_process)
+    sql = f"""
+        SELECT
+            id AS game_id,
+            log
+        FROM
+            game
+        WHERE
+            id IN ({placeholders})
+    """
+    rs = logsite_db().select(sql, ids_to_process)
     values = []
     for r in rs:
         entries = process_log(r['log'])
@@ -83,6 +104,26 @@ def process_logs() -> None:
     sql = 'INSERT INTO _game_card_person (game_id, mtgo_username, name) VALUES '
     sql += ', '.join('(' + ', '.join(str(sqlescape(v)) for v in vs) + ')' for vs in values)
     logsite_db().execute(sql)
+
+def next_ids():
+    sql = """
+        SELECT
+            id AS game_id
+        FROM
+            game
+        WHERE
+            id NOT IN (
+                SELECT
+                    game_id
+                FROM
+                    _game_card_person
+            )
+        ORDER BY
+            id
+        LIMIT
+            10000
+    """
+    return logsite_db().values(sql)
 
 def process_log(log: str) -> List[Tuple[str, str]]:
     return re.findall(r'(\w+) (?:casts|plays) \[([^\]]+)\]', log, re.MULTILINE)

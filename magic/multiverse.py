2160a4ad1136b8d753ab07912b03b5624b0f3241
Thomas David Baker <bakert@gmail.com>
2019-11-24 12:39:14 +0000

    Ugly hack while Scryfall set data is buggy.

--- magic/multiverse.py
+++ magic/multiverse.py
@@ -453,7 +453,8 @@ def database2json(propname: str) -> str:
 
 def date2int(s: str, name: str) -> Union[str, float]:
     if name == 'released_at':
-        return dtutil.parse_to_ts(s, '%Y-%m-%d', dtutil.WOTC_TZ)
+        print(f'parsing {s}')
+        return dtutil.parse_to_ts(s.replace('2109', '2019'), '%Y-%m-%d', dtutil.WOTC_TZ)
     return s
 
 # I'm not sure this belong here, but it's here for now.

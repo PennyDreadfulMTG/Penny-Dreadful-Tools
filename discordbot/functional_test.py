e28edc8c7a41026669b721d58ce32be6436ebf48
Katelyn Gigante <clockwork.singularity@gmail.com>
2020-01-08 11:08:19 +1100

    Typing

--- discordbot/functional_test.py
+++ discordbot/functional_test.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Any, Dict, List, Tuple, cast
 
 import discord
 import pytest
@@ -16,11 +17,15 @@ def discordbot() -> Bot:
     return bot
 
 class TestContext(MtgContext):
-    def __init__(self, **attrs) -> None:
+    def __init__(self, **attrs: Any) -> None:  # pylint: disable=super-init-not-called
         self.sent = False
+        self.sent_args = False
+        self.sent_file = False
 
-    async def send(self, *args, **kwargs) -> None:
+    async def send(self, *args: Any, **kwargs: Any) -> None: # pylint: disable=arguments-differ
         self.sent = True
+        self.sent_args = bool(args)
+        self.sent_file = 'file' in kwargs.keys()
 
     def typing(self) -> 'TestContext':
         return self
@@ -28,20 +33,26 @@ class TestContext(MtgContext):
     def __enter__(self) -> None:
         pass
 
-    def __exit__(self, _, __, ___):
+    async def __aenter__(self) -> 'TestContext':
+        return self
+
+    def __exit__(self, exc_type, exc, tb):  # type: ignore
+        pass
+
+    async def __aexit__(self, exc_type, exc, tb) -> None:  # type: ignore
         pass
 
 async def card(param: str) -> Card:
     ctx = TestContext()
-    return await CardConverter.convert(ctx, param)
+    return await cast(Card, CardConverter.convert(ctx, param))
 
 
-def get_params():
-    async def params():
+def get_params() -> List[Tuple]:
+    async def params() -> List[Tuple]:
         return [
             ('art', {'c': await card('Island')}),
             ('barbs', {}),
-            # ('downtimes', {}),
+            ('downtimes', {}),
             ('echo', {'args': 'test string!'}),
             ('explain', {'thing': None}),
             ('explain', {'thing': 'bugs'}),
@@ -52,24 +63,36 @@ def get_params():
             ('legal', {'c': await card('Island')}),
             ('legal', {'c': await card('Black Lotus')}),
             ('oracle', {'c': await card('Dark Ritual')}),
-            ('p1p1', {}),
+            pytest.param('p1p1', {}, marks=pytest.mark.functional),
             ('patreon', {}),
             ('price', {'c': await card('Gleemox')}),
             ('rotation', {}),
-            # ('rulings', {'c': await card('Worldknit')}),
-
-
+            pytest.param('rhinos', {}, marks=pytest.mark.functional),
+            ('rulings', {'c': await card('Worldknit')}),
+            ('search', {'args': 'f:pd'}),
+            ('status', {}),
+            ('time', {'args': 'AEST'}),
+            ('tournament', {}),
+            ('version', {}),
+            ('whois', {'args': 'silasary'}),
+            ('whois', {'args': 'kaet'}),
+            ('whois', {'args': '<@154363842451734528>'}),
+            ('whois', {'args': '<@!224755717767299072>'})
         ]
     loop = asyncio.new_event_loop()
     return loop.run_until_complete(params())
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize('cmd, kwargs', get_params())
-async def test_command(discordbot: Bot, cmd: str, kwargs) -> None:
+async def test_command(discordbot: Bot, cmd: str, kwargs: Dict[str, Any]) -> None: # pylint: disable=redefined-outer-name
     command: discord.ext.commands.Command = discordbot.all_commands[cmd]
     ctx = TestContext()
     ctx.bot = discordbot
     ctx.message = Container()
     ctx.message.channel = Container({'id': '1'})
+    ctx.message.channel.typing = ctx.typing
+    ctx.message.channel.send = ctx.send
+    ctx.author = Container()
+    ctx.author.mention = '<@111111111111>'
     await command.callback(ctx, **kwargs)
     assert ctx.sent

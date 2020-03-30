eae743255b32e3fe80211acb227ac4f35ff596b3
Thomas David Baker <bakert@gmail.com>
2020-03-29 22:23:50 -0700

    Lint.

--- decksite/data/elo_test.py
+++ decksite/data/elo_test.py
@@ -1,12 +1,10 @@
-import pytest
-
 from decksite.data import elo
 
 
 def test_elo() -> None:
-    assert(elo.expected(elo.STARTING_ELO, elo.STARTING_ELO) == 0.5)
-    assert(elo.expected(10000, 1) > 0.99)
-    assert(elo.expected(1, 10000) < 0.01)
-    assert(elo.adjustment(elo.STARTING_ELO, elo.STARTING_ELO) == elo.K_FACTOR / 2)
-    assert(elo.adjustment(10000, 1) == 1)
-    assert(elo.adjustment(1, 10000) == elo.K_FACTOR)
+    assert elo.expected(elo.STARTING_ELO, elo.STARTING_ELO) == 0.5
+    assert elo.expected(10000, 1) > 0.99
+    assert elo.expected(1, 10000) < 0.01
+    assert elo.adjustment(elo.STARTING_ELO, elo.STARTING_ELO) == elo.K_FACTOR / 2
+    assert elo.adjustment(10000, 1) == 1
+    assert elo.adjustment(1, 10000) == elo.K_FACTOR

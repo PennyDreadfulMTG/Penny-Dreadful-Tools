7146d80885dfaded231ebadd15f47a7f9d0ca140
Katelyn Gigante <clockwork.singularity@gmail.com>
2020-03-31 02:03:45 +1100

    Show python version in !version

--- discordbot/commands/version.py
+++ discordbot/commands/version.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 from discord.ext import commands
 
@@ -11,4 +12,4 @@ async def version(ctx: MtgContext) -> None:
     """Display the current version numbers"""
     commit = subprocess.check_output(['git', 'rev-parse', 'HEAD'], universal_newlines=True).strip('\n').strip('"')
     scryfall = database.last_updated()
-    return await ctx.send(f'I am currently running mtgbot version `{commit}`, and scryfall last updated `{scryfall}`')
+    return await ctx.send(f'I am currently running mtgbot version `{commit}`, and scryfall last updated `{scryfall}`\nPython `{sys.version}`')

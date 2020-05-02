d0944f9a21a99961d64f82f48d0f6498a8793d1c
Thomas David Baker <bakert@gmail.com>
2020-05-02 16:09:25 -0700

    Support Scryfall deckbuilder format.

--- magic/decklist_test.py
+++ magic/decklist_test.py
@@ -546,3 +546,37 @@ def test_parse_tappedout_commander() -> None:
     assert sum(d['maindeck'].values()) == 100
     assert len(d['maindeck']) == 83
     assert len(d['sideboard']) == 0
+
+def test_parse_scryfall() -> None:
+    s = """
+        4 Deeproot Champion
+        2 Quench
+        1 Spell Rupture
+        3 Terramorphic Expanse
+        4 Quirion Dryad
+        3 Hooting Mandrills
+        1 Snapback
+        4 Forest
+        1 Unsummon
+        4 Gitaxian Probe
+        2 Prohibit
+        3 Gush
+        4 Peek
+        2 Censor
+        4 Mental Note
+        2 Negate
+        3 Treasure Cruise
+        4 Evolving Wilds
+        9 Island
+
+        // Sideboard
+        4 Scrabbling Claws
+        3 Naturalize
+        3 Snapback
+        4 Invasive Surgery
+        1 Boomerang
+    """
+    s = textwrap.dedent(s)
+    d = decklist.parse(s)
+    assert sum(d['maindeck'].values()) == 60
+    assert sum(d['sideboard'].values()) == 15

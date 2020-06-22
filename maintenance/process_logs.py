39bdbf2efb2e31818dec9c767eb845fefb1c2e4f
Thomas David Baker <bakert@gmail.com>
2020-06-21 20:29:30 -0700

    Turn off process_logs - it doesn't quite work and it isn't in use.

--- maintenance/process_logs.py
+++ maintenance/process_logs.py
@@ -1,5 +1,6 @@
 from analysis import analysis
 
 
-def run() -> None:
+# This code forever tries to process logs with "casts" or "plays". Needs work if it is to be re-enabled. See analysis.py.
+def ad_hoc() -> None:
     analysis.process_logs()

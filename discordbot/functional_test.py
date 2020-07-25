3aa3b91cd42527109931d711d812d11e4a7d1412
Katelyn Gigante <clockwork.singularity@gmail.com>
2020-07-25 23:11:39 +1000

    Handle 4-letter setcodes, and better tests

--- discordbot/functional_test.py
+++ discordbot/functional_test.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Any, Dict, List, Tuple, cast
+from typing import Any, Dict, List, Tuple, cast, Optional
 
 import discord
 import pytest
@@ -21,11 +21,13 @@ class ContextForTests(MtgContext):
         self.sent = False
         self.sent_args = False
         self.sent_file = False
+        self.content = None
 
-    async def send(self, *args: Any, **kwargs: Any) -> None: # pylint: disable=signature-differs
+    async def send(self, content: Optional[str] = None, *args: Any, **kwargs: Any) -> None: # pylint: disable=signature-differs
         self.sent = True
         self.sent_args = bool(args)
         self.sent_file = 'file' in kwargs.keys()
+        self.content = content
 
     def typing(self) -> 'ContextForTests':
         return self
@@ -50,40 +52,42 @@ async def card(param: str) -> Card:
 def get_params() -> List[Tuple]:
     async def params() -> List[Tuple]:
         return [
-            ('art', {'c': await card('Island')}),
-            ('barbs', {}),
-            ('echo', {'args': 'test string!'}),
-            ('explain', {'thing': None}),
-            ('explain', {'thing': 'bugs'}),
-            ('flavor', {'c': await card('Island')}), # No flavor
-            ('flavor', {'c': await card('Horned Turtle')}),  # Tasty Flavor
-            ('flavor', {'c': await card('Gruesome Menagerie|RNA')}),  # Spicy Flavor
-            ('history', {'c': await card('Necropotence')}),
-            ('legal', {'c': await card('Island')}),
-            ('legal', {'c': await card('Black Lotus')}),
-            ('oracle', {'c': await card('Dark Ritual')}),
-            pytest.param('p1p1', {}, marks=pytest.mark.functional),
-            ('patreon', {}),
-            ('price', {'c': await card('Gleemox')}),
-            ('rotation', {}),
-            pytest.param('rhinos', {}, marks=pytest.mark.functional),
-            ('rulings', {'c': await card('Worldknit')}),
-            ('search', {'args': 'f:pd'}),
-            ('status', {}),
-            ('time', {'args': 'AEST'}),
-            ('tournament', {}),
-            ('version', {}),
-            ('whois', {'args': 'silasary'}),
-            ('whois', {'args': 'kaet'}),
-            ('whois', {'args': '<@154363842451734528>'}),
-            ('whois', {'args': '<@!224755717767299072>'})
+            ('art', {'c': await card('Island')}, None),
+            ('barbs', {}, None),
+            ('echo', {'args': 'test string!'}, None),
+            ('explain', {'thing': None}, None),
+            ('explain', {'thing': 'bugs'}, None),
+            ('flavor', {'c': await card('Falling Star')}, 'No flavor text available'), # No flavor
+            ('flavor', {'c': await card('Dwarven Pony')}, 'likes to eat meat'),  # Meaty flavor
+            ('flavor', {'c': await card('Gruesome Menagerie|grn')}, 'Variety is also the spice of death.'),  # Spicy flavor
+            ('flavor', {'c': await card('capital offense|UST')}, 'part basket case, all lowercase.'),  # Capital flavor
+            ('flavor', {'c': await card('Reliquary Tower|plgs')}, 'Archmage Vintra'),  # Long set code
+            ('history', {'c': await card('Necropotence')}, None),
+            ('legal', {'c': await card('Island')}, None),
+            ('legal', {'c': await card('Black Lotus')}, None),
+            ('oracle', {'c': await card('Dark Ritual')}, None),
+            pytest.param('p1p1', {}, None, marks=pytest.mark.functional),
+            ('patreon', {}, None),
+            ('price', {'c': await card('Gleemox')}, None),
+            ('rotation', {}, None),
+            pytest.param('rhinos', {}, None, marks=pytest.mark.functional),
+            ('rulings', {'c': await card('Worldknit')}, None),
+            ('search', {'args': 'f:pd'}, None),
+            ('status', {}, None),
+            ('time', {'args': 'AEST'}, None),
+            ('tournament', {}, None),
+            ('version', {}, None),
+            ('whois', {'args': 'silasary'}, None),
+            ('whois', {'args': 'kaet'}, None),
+            ('whois', {'args': '<@154363842451734528>'}, None),
+            ('whois', {'args': '<@!224755717767299072>'}, None)
         ]
     loop = asyncio.new_event_loop()
     return loop.run_until_complete(params())
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('cmd, kwargs', get_params())
-async def test_command(discordbot: Bot, cmd: str, kwargs: Dict[str, Any]) -> None: # pylint: disable=redefined-outer-name
+@pytest.mark.parametrize('cmd, kwargs, expected_content', get_params())
+async def test_command(discordbot: Bot, cmd: str, kwargs: Dict[str, Any], expected_content: str) -> None: # pylint: disable=redefined-outer-name
     command: discord.ext.commands.Command = discordbot.all_commands[cmd]
     ctx = ContextForTests()
     ctx.bot = discordbot
@@ -95,3 +99,5 @@ async def test_command(discordbot: Bot, cmd: str, kwargs: Dict[str, Any]) -> Non
     ctx.author.mention = '<@111111111111>'
     await command.callback(ctx, **kwargs)
     assert ctx.sent
+    if expected_content is not None:
+        assert expected_content in ctx.content

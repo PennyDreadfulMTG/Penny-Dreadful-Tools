120cf977ed41d5483f04a21152bdd1ba299315c4
Thomas David Baker <bakert@gmail.com>
2020-09-03 20:38:28 -0700

    Lint.

--- decksite/views/pd500.py
+++ decksite/views/pd500.py
@@ -12,7 +12,7 @@ from shared import dtutil
 class PD500(View):
     def __init__(self, tournament_winning_decks: List[Deck]) -> None:
         super().__init__()
-        people = set([d.person for d in tournament_winning_decks])
+        people = set(d.person for d in tournament_winning_decks)
         self.people_with_byes = [{'person': person, 'url': url_for('.person', mtgo_username=person)} for person in people]
         self.people_with_byes = sorted(self.people_with_byes, key=lambda k: k['person'])
         self.next_pd500_date = dtutil.display_date_with_date_and_year(tournaments.next_pd500_date())

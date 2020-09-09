89875de61735e3390bac407474ea5dbc8645ec4d
Thomas David Baker <bakert@gmail.com>
2020-09-08 18:14:31 -0700

    Add resources to typeahead search.

--- maintenance/typeahead.py
+++ maintenance/typeahead.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 from flask import url_for
 
 from decksite.data import archetype, person
-from magic import oracle
+from magic import fetcher, oracle
 from shared import configuration
 
 REQUIRES_APP_CONTEXT = True
@@ -12,6 +12,13 @@ REQUIRES_APP_CONTEXT = True
 def run() -> None:
     urls = archetypes() + cards() + people()
     write_typeahead(urls)
+
+def archetypes() -> List[Dict[str, str]]:
+    urls = []
+    archs = archetype.load_archetypes()
+    for a in archs:
+        urls.append({'name': a.name, 'type': 'Archetype', 'url': url_for('archetype', archetype_id=a.name)})
+    return urls
 
 def cards() -> List[Dict[str, str]]:
     urls = []
@@ -25,11 +32,11 @@ def people() -> List[Dict[str, str]]:
         urls.append({'name': p.name, 'type': 'Person', 'url': url_for('person', mtgo_username=p.name)})
     return urls
 
-def archetypes() -> List[Dict[str, str]]:
+def resources() -> List[Dict[str, str]]:
     urls = []
-    archs = archetype.load_archetypes()
-    for a in archs:
-        urls.append({'name': a.name, 'type': 'Archetype', 'url': url_for('archetype', archetype_id=a.name)})
+    for category, entries in fetcher.resources().items():
+        for name, url in entries.items():
+            urls.append({'name': f'{category} – {name}', 'type': 'Resource', 'url': url})
     return urls
 
 def write_typeahead(urls: List[Dict[str, str]]) -> None:

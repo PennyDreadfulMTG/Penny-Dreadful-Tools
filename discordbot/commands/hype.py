271bd78061052c65ed6c294e46fa7d96db7eb9ca
Katelyn Gigante <clockwork.singularity@gmail.com>
2020-09-26 07:32:03 +1000

    Make sure !hype doesn't cause unexpected problems

--- discordbot/commands/hype.py
+++ discordbot/commands/hype.py
@@ -2,19 +2,22 @@ import datetime
 
 from discord.ext import commands
 
-from discordbot import bot
+from discordbot import bot # This is a circular import
 from discordbot.command import MtgContext
 from magic import rotation
 from shared import dtutil
 
 
+@commands.is_owner()
 @commands.command()
 async def hype(ctx: MtgContext) -> None:
-    """Display the  latest rotation hype message."""
+    """Display the latest rotation hype message."""
     until_rotation = rotation.next_rotation() - dtutil.now()
     last_run_time = rotation.last_run_time()
+    msg = None
     if until_rotation < datetime.timedelta(7) and last_run_time is not None:
         msg = await bot.rotation_hype_message()
+        rotation.clear_redis()
     if msg:
         await ctx.send(msg)
     else:

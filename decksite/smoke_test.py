be515e01d7600a6339e2039f28d13521550c020b
Katelyn Gigante <clockwork.singularity@gmail.com>
2020-10-02 18:05:22 +1000

    Post-rotation tweaks

--- decksite/smoke_test.py
+++ decksite/smoke_test.py
@@ -1,4 +1,5 @@
 import unittest
+from flask.helpers import url_for
 
 import pytest
 
@@ -14,3 +15,16 @@ class DecksiteSmokeTest(unittest.TestCase):
     def test_some_pages(self) -> None:
         for path in ['/', '/people/', '/cards/', '/cards/Unsummon/', '/competitions/', '/competitions/', '/tournaments/', '/resources/', '/bugs/', '/signup/', '/report/']:
             self.tester.response_test(path, 200)
+
+    def test_trailing_slashes(self) -> None:
+        urls = [url_for(rule.endpoint) for rule in APP.url_map.iter_rules()]
+        for url in urls:
+            if not url.startswith('/api/'):
+                assert url.endswith('/')
+
+    @pytest.mark.xfail('We need to fix this')
+    def test_api_no_trailing_slashes(self) -> None:
+        urls = [url_for(rule.endpoint) for rule in APP.url_map.iter_rules()]
+        for rule in APP.url_map.iter_rules():
+            if url.startswith('/api/'):
+                assert not url.endswith('/')

eb78c67b5cd2eaeb0cdae8cce3397519e4d9aab5
Thomas David Baker <bakert@gmail.com>
2021-03-11 15:56:07 -0800

    Monkey around with decksite.database to allow for a once-per-test test db

--- decksite/database.py
+++ decksite/database.py
@@ -6,6 +6,7 @@ from shared import configuration, logger
 from shared.container import Container
 from shared.database import Database, get_database
 
+TEST_CONTEXT = Container()
 
 def db() -> Database:
     if has_request_context():  # type: ignore
@@ -13,33 +14,36 @@ def db() -> Database:
     elif g:
         ctx = g
     else:
-        ctx = Container() # Fallback context for testing.
+        ctx = TEST_CONTEXT # Fallback context for testing.
     if not hasattr(ctx, 'database'):
         ctx.database = get_database(configuration.get_str('decksite_database')) # type: ignore
     return ctx.database # type: ignore
 
-def setup() -> None:
+def setup_in_app_context() -> None:
     # pylint: disable=import-outside-toplevel
     from decksite import APP
     with APP.app_context():  # type: ignore
-        db().execute('CREATE TABLE IF NOT EXISTS db_version (version INTEGER UNIQUE NOT NULL)')
-        version = db_version()
-        patches = os.listdir('decksite/sql')
-        patches.sort(key=lambda n: int(n.split('.')[0]))
-        for fn in patches:
-            path = os.path.join('decksite/sql', fn)
-            n = int(fn.split('.')[0])
-            if version < n:
-                logger.warning('Patching database to v{0}'.format(n))
-                fh = open(path, 'r')
-                sql = fh.read()
-                for stmt in sql.split(';'):
-                    if stmt.strip() != '':
-                        db().execute(stmt)
-                fh.close()
-                db().execute('INSERT INTO db_version (version) VALUES ({n})'.format(n=n))
+        setup()
+
+def setup() -> None:
+    db().execute('CREATE TABLE IF NOT EXISTS db_version (version INTEGER UNIQUE NOT NULL)')
+    version = db_version()
+    patches = os.listdir('decksite/sql')
+    patches.sort(key=lambda n: int(n.split('.')[0]))
+    for fn in patches:
+        path = os.path.join('decksite/sql', fn)
+        n = int(fn.split('.')[0])
+        if version < n:
+            logger.warning('Patching database to v{0}'.format(n))
+            fh = open(path, 'r')
+            sql = fh.read()
+            for stmt in sql.split(';'):
+                if stmt.strip() != '':
+                    db().execute(stmt)
+            fh.close()
+            db().execute('INSERT INTO db_version (version) VALUES ({n})'.format(n=n))
 
 def db_version() -> int:
     return db().value('SELECT version FROM db_version ORDER BY version DESC LIMIT 1', [], 0)
 
-setup()
+setup_in_app_context()

241b00537c3601db39f593249c6c6178b21f7c92
Thomas David Baker <bakert@gmail.com>
2021-03-11 15:54:28 -0800

    Introduce the concept of a on-per-unit-test test db

---
+++ decksite/testutil.py
@@ -0,0 +1,11 @@
+from decksite import database
+from decksite.database import db
+from shared import configuration
+
+def setup_test_db():
+    name = configuration.get_str('decksite_test_database')
+    configuration.CONFIG['decksite_database'] = name
+    db().execute(f'DROP DATABASE IF EXISTS {name}')
+    db().execute(f'CREATE DATABASE {name}')
+    db().execute(f'USE {name}')
+    database.setup()

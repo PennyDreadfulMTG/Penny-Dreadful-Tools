5efd1346ce81eac4610f4e488bd5d6e781fa2070
Thomas David Baker <bakert@gmail.com>
2021-06-19 16:03:04 -0700

    Types

--- magic/models/competition.py
+++ magic/models/competition.py
@@ -1,5 +1,9 @@
+from typing import Any, Dict
+
 from shared.container import Container
 
 
 class Competition(Container):
-    pass
+    def __init__(self, params: Dict[str, Any]) -> None:
+        super().__init__(params)
+        self.base_archetype_data: Dict[str, int] = {}

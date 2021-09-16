b6a71257580930e91d7cfbd491d45409dd93352b
Thomas David Baker <bakert@gmail.com>
2021-09-15 22:19:36 -0700

    Fix get_season_id tests

--- decksite/data/season_test.py
+++ decksite/data/season_test.py
@@ -3,9 +3,10 @@ from shared import dtutil
 from shared.pd_exception import InvalidDataException
 
 
-def test_load_season_id() -> None:
-    dt = dtutil.parse('2021-09-12 00:00:00', '%Y-%m-%d %H:%M:%S', dtutil.WOTC_TZ)
-    assert season.get_season_id(dt) == 21
+def test_get_season_id() -> None:
+    # These tests check pretty old dates because the test db only has up to season 7.
+    dt = dtutil.parse('2017-09-12 00:00:00', '%Y-%m-%d %H:%M:%S', dtutil.WOTC_TZ)
+    assert season.get_season_id(dt) == 5
     dt = dtutil.parse('2016-11-01 00:00:00', '%Y-%m-%d %H:%M:%S', dtutil.WOTC_TZ)
     assert season.get_season_id(dt) == 2
     found = False

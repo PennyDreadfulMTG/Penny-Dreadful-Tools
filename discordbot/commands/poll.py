d0774f6d269d8949ebd45b9ce45ae9db3acdea50
Katelyn Gigante <clockwork.singularity@gmail.com>
2021-10-15 15:34:50 +1100

    Accidentally commited a file that shouldn't exist

--- discordbot/commands/poll.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-
-from discordbot.command import MtgContext
-
-async def poll(ctx: MtgContext) -> None:
-    msg = ctx.message.reference
-    if msg is None:
-        await ctx.reply('You need to invoke this while replying to a message.')
-        return

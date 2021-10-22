cec56802a7e6dd25a23c6a5f9054bcd80e0cc3be
Katelyn Gigante <clockwork.singularity@gmail.com>
2021-10-22 19:38:21 +1100

    Fix tests

--- discordbot/emoji_test.py
+++ discordbot/emoji_test.py
@@ -1,4 +1,5 @@
 from discordbot import emoji
+from magic import seasons
 from shared.container import Container
 
 
@@ -6,7 +7,7 @@ def test_info_emoji() -> None:
     r = emoji.info_emoji(Container({'name': 'Island', 'bugs': []}), verbose=False, show_legality=True, no_rotation_hype=True)
     assert r == ':white_check_mark:'
     r = emoji.info_emoji(Container({'name': 'Black Lotus', 'bugs': []}), verbose=True, show_legality=True, no_rotation_hype=True)
-    assert r == ':no_entry_sign: (not legal in PD)'
+    assert r == f':no_entry_sign: (not legal in {seasons.current_season_name()})'
     r = emoji.info_emoji(Container({'name': 'Ice Cauldron', 'bugs': [{}, {}]}), verbose=False, show_legality=False, no_rotation_hype=True)
     assert r == ':lady_beetle:'
     r = emoji.info_emoji(Container({'name': 'Mountain', 'bugs': []}), verbose=False, show_legality=False, no_rotation_hype=True)

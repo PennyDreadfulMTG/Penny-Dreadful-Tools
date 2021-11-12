7928aa938a4e5c2ece680c155d8f4d6aa29bfa35
Thomas David Baker <bakert@gmail.com>
2021-11-11 17:17:25 -0800

    Warn you on the sign up page if you are already signed up

--- decksite/views/signup.py
+++ decksite/views/signup.py
@@ -1,16 +1,22 @@
 from typing import Optional
 
+from flask import url_for
 from flask_babel import gettext
 
 from decksite.data.form import Form
 from decksite.views.decklist_form import DecklistForm
+from magic.models import Deck
 
 
 # pylint: disable=no-self-use
 class SignUp(DecklistForm):
-    def __init__(self, form: Form, is_closed: bool, person_id: Optional[int]) -> None:
+    def __init__(self, form: Form, is_closed: bool, person_id: Optional[int], d: Optional[Deck]) -> None:
         super().__init__(form, person_id)
         self.is_closed = is_closed
+        if d:
+            deck_url = url_for('deck', deck_id=d.id)
+            retire_url = url_for('retire')
+            self.signed_up_msg_safe = f'You are already signed up to the league with <a href="{deck_url}">{d.name}</a>. Do you want to <a href="{retire_url}">Retire?</a>'
 
     def page_title(self) -> str:
         return '{league} Sign Up'.format(league=self.league['name'])

80902e411c41a9c195dc9b3692c465079ee7c1fc
Katelyn Gigante <clockwork.singularity@gmail.com>
2021-12-07 03:34:12 +1100

    Make !reboot actually reboot

--- discordbot/commands/restartbot.py
+++ discordbot/commands/restartbot.py
@@ -1,3 +1,5 @@
+import sys
+
 from dis_snek.models.checks import is_owner
 from dis_snek.models.command import check, message_command
 
@@ -10,3 +12,4 @@ async def restartbot(ctx: MtgContext) -> None:
     """Restart the bot."""
     await ctx.send('Rebooting!')
     await ctx.bot.stop()
+    sys.exit(0)

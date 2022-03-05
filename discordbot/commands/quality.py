bc907f4f3f69c08a2456a48979aa575f54542b4d
Katelyn Gigante <clockwork.singularity@gmail.com>
2022-03-05 15:13:35 +1100

    Make !quality work again
    Fixes #9609

--- discordbot/commands/quality.py
+++ discordbot/commands/quality.py
@@ -6,6 +6,6 @@ from discordbot.command import MtgContext
 @message_command('quality')
 async def quality(ctx: MtgContext, product: CMD_BODY = None) -> None:
     """A reminder about everyone's favorite way to play digital Magic"""
-    if product is None:
+    if not product:
         product = 'Magic Online'
     await ctx.send(f'**{product}** is a Quality™ Program.')

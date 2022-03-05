051a236aabc928d87e634f3727cfcbfdbc4077b1
Katelyn Gigante <clockwork.singularity@gmail.com>
2022-03-05 15:19:41 +1100

    Use Context in /mana
    Fix #9590

--- discordbot/commands/mana.py
+++ discordbot/commands/mana.py
@@ -11,6 +11,6 @@ async def mana(ctx: MtgContext) -> None:
     """Get Dr. Karsten's advice on number of colored sources of mana required."""
     with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'img/mana-frank.png'), 'rb') as f:
         img = File(f)
-        await ctx.channel.send(file=img)
+        await ctx.send(file=img)
 
 m_mana = message_command(mana.callback)

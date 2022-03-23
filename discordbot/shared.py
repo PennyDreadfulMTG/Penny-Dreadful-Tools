dc6f6fcab0047144ebb74992a9b6bc59c10754a3
Katelyn Gigante <clockwork.singularity@gmail.com>
2022-03-23 13:23:35 +1100

    Use SendableContext from dis-taipan

--- discordbot/shared.py
+++ discordbot/shared.py
@@ -28,29 +28,3 @@ def channel_id(ctx: Union[Context, TYPE_MESSAGEABLE_CHANNEL, None]) -> Optional[
             return None
         return ctx.channel.id
     return ctx.id
-
-class SendableContext(Protocol):
-    channel: TYPE_MESSAGEABLE_CHANNEL
-    bot: Snake
-    invoked_name: str
-
-    author: Union[Member, User]
-    guild_id: Snowflake_Type
-    message: Message
-
-    async def send(
-        self,
-        content: Optional[str] = None,
-        embeds: Optional[Union[List[Union['Embed', dict]], Union['Embed', dict]]] = None,
-        components: Optional[
-            Union[List[List[Union['BaseComponent', dict]]], List[Union['BaseComponent', dict]], 'BaseComponent', dict]
-        ] = None,
-        stickers: Optional[Union[List[Union['Sticker', 'Snowflake_Type']], 'Sticker', 'Snowflake_Type']] = None,
-        allowed_mentions: Optional[Union['AllowedMentions', dict]] = None,
-        reply_to: Optional[Union['MessageReference', 'Message', dict, 'Snowflake_Type']] = None,
-        file: Optional[Union['File', 'IOBase', 'Path', str]] = None,
-        tts: bool = False,
-        flags: Optional[Union[int, 'MessageFlags']] = None,
-        **kwargs: Any,
-    ) -> 'Message':
-        ...

14e1a6da1eb0901d5ae00b6b0a64546d60904ad3
Thomas David Baker <bakert@gmail.com>
2022-03-26 11:22:15 -0700

    Make gbug a slash command with title and body, fix bug
    Fixes #9912.

--- discordbot/commands/bug.py
+++ discordbot/commands/bug.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from dis_snek import Scale, Snake
-from dis_snek.models import CMD_BODY, OptionTypes, message_command, slash_command, slash_option
+from dis_snek.models import OptionTypes, slash_command, slash_option
 
 from discordbot.command import MtgContext
 from shared import repo
@@ -22,9 +22,14 @@ class Bug(Scale):
         else:
             await ctx.send('Issue has been reported at <{url}>'.format(url=issue.html_url))
 
-    @message_command('gbug')
-    async def gatherlingbug(ctx: MtgContext, text: CMD_BODY) -> None:
+    @slash_command('gbug')
+    @slash_option('title', 'One sentence description of the issue', OptionTypes.STRING, required=True)
+    @slash_option('body', 'More info', OptionTypes.STRING)
+    async def gatherlingbug(self, ctx: MtgContext, title: str, body: Optional[str] = None) -> None:
         """Report a Gatherling bug."""
+        text = title
+        if body:
+            text += f'\n\n{body}'
         issue = repo.create_issue(text, str(ctx.author), 'Discord', 'PennyDreadfulMTG/gatherling')
         if issue is None:
             await ctx.send('Report Gatherling issues at <https://github.com/PennyDreadfulMTG/gatherling/issues/new>')

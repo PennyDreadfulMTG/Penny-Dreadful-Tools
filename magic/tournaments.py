f387295f1f7f1079bc9f9bcd60b5ba38ce4be5cd
Thomas David Baker <bakert@gmail.com>
2022-05-03 13:24:01 -0700

    Reinstate PD Tuesdays

--- magic/tournaments.py
+++ magic/tournaments.py
@@ -20,8 +20,9 @@ SAT = 2
 APAC = 3
 SUN = 4
 MON = 5
-EU_WED = 6
-THU = 7
+TUE = 6
+EU_WED = 7
+THU = 8
 
 class TimeDirection(Enum):
     BEFORE = 1
@@ -80,9 +81,10 @@ def get_all_next_tournament_dates(start: datetime.datetime, index: int = 0) -> L
     apds_time = (APAC, 'APAC Penny Dreadful Sundays', rrule.rrule(rrule.WEEKLY, byhour=16, byminute=0, bysecond=0, dtstart=apac_start, until=until, byweekday=rrule.SU)[index])
     pds_time = (SUN, 'Penny Dreadful Sundays', rrule.rrule(rrule.WEEKLY, byhour=13, byminute=30, bysecond=0, dtstart=start, until=until, byweekday=rrule.SU)[index])
     pdm_time = (MON, 'Penny Dreadful Mondays', rrule.rrule(rrule.WEEKLY, byhour=19, byminute=0, bysecond=0, dtstart=start, until=until, byweekday=rrule.MO)[index])
+    pdtue_time = (TUE, 'Penny Dreadful Tuesdays', rrule.rrule(rrule.WEEKLY, byhour=19, byminute=0, bysecond=0, dtstart=start, until=until, byweekday=rrule.TU)[index])
     pdew_time = (EU_WED, 'Penny Dreadful EU Wednesdays', rrule.rrule(rrule.WEEKLY, byhour=18, byminute=0, bysecond=0, dtstart=eu_start, until=until, byweekday=rrule.WE)[index])
     pdthu_time = (THU, 'Penny Dreadful Thursdays', rrule.rrule(rrule.WEEKLY, byhour=19, byminute=0, bysecond=0, dtstart=start, until=until, byweekday=rrule.TH)[index])
-    return [pdfnm_time, pdsat_time, apds_time, pds_time, pdm_time, pdew_time, pdthu_time]
+    return [pdfnm_time, pdsat_time, apds_time, pds_time, pdm_time, pdtue_time, pdew_time, pdthu_time]
 
 # Note: this may be in the past. It always gives the date for the current season.
 # Note: if the start date of next season is not known then the date of the PD 500 cannot be known and in such a case this return None.

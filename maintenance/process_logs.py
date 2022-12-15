e637c7058d21976a6f5b84d02c89f8cf48ed0b2b
Katelyn Gigante <clockwork.singularity@gmail.com>
2022-12-15 13:13:04 +1100

    Don't import logsite unless we plan on running analysis.

--- maintenance/process_logs.py
+++ maintenance/process_logs.py
@@ -1,6 +1,4 @@
-from analysis import analysis
-
-
 # This code forever tries to process logs with "casts" or "plays". Needs work if it is to be re-enabled. See analysis.py.
 def ad_hoc() -> None:
+    from analysis import analysis
     analysis.process_logs()

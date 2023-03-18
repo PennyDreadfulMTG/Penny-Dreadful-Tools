ed53d7d1a1589d9c38fe7de637baca6d1de70d32
Katelyn Gigante <clockwork.singularity@gmail.com>
2023-03-18 23:43:42 +1100

    Clean up pricefile badnames, and add a few more to ther list

--- price_grabber/parser.py
+++ price_grabber/parser.py
@@ -5,6 +5,7 @@ from typing import Dict, List, Tuple
 
 from magic import card, card_price, multiverse
 from magic.database import db
+from shared import logger
 from shared.pd_exception import DatabaseException, InvalidDataException
 
 PriceListType = List[Tuple[str, str, str]]
@@ -17,8 +18,9 @@ def parse_cardhoarder_prices(s: str) -> PriceListType:
     for line in s.splitlines()[2:]:  # Skipping date and header line.
         if line.count('\t') != 6:
             raise InvalidDataException('Bad line (cardhoarder): {line}'.format(line=line))
-        _mtgo_id, mtgo_set, _mtgjson_set, set_number, name, p, quantity = line.split('\t')  # pylint: disable=unused-variable
+        _mtgo_id, mtgo_set, _mtgjson_set, set_number, name, p, quantity = line.split('\t')
         name = html.unescape(name.strip())
+        name = repair_name(name)
         if int(quantity) > 0 and not mtgo_set.startswith('CH-') and mtgo_set != 'VAN' and mtgo_set != 'EVENT' and not re.search(r'(Booster|Commander Deck|Commander:|Theme Deck|Draft Pack|Duel Decks|Reward Pack|Intro Pack|Tournament Pack|Premium Deck Series:|From the Vault)', name):
             details.append((name, p, mtgo_set))
     return [(name_lookup(name), html.unescape(p.strip()), mtgo_set) for name, p, mtgo_set in details if name_lookup(name) is not None]
@@ -34,20 +36,28 @@ def parse_mtgotraders_prices(s: str) -> PriceListType:
         in_stock = in_stock_str == 'Yes'
         if name.endswith('(a)') or name.endswith('(b)'):  # Guildgates
             name = name[:-4]
-        if name == 'Tura Kenner':
-            name = 'Tura Kennerüd, Skyknight'
+        name = repair_name(name)
         if float(p) <= card_price.MAX_PRICE_TIX and in_stock and not is_exceptional_name(name):
             details.append((name, p, mtgo_set))
     return [(name_lookup(name), p, mtgo_set) for name, p, mtgo_set in details if name_lookup(name) is not None]
+
+def repair_name(name: str) -> str:
+    if name == 'Tura Kenner':
+        name = 'Tura Kennerüd, Skyknight'
+    elif name == 'Lurrus of the Dream Den':
+        name = 'Lurrus of the Dream-Den'
+    elif name == 'Kongming, Sleeping Dragon':
+        name = 'Kongming, "Sleeping Dragon"'
+    elif name == 'Pang Tong, Young Phoenix':
+        name = 'Pang Tong, "Young Phoenix"'
+    elif name.endswith(' - Sketch'):
+        name = name[:-9]
+    return name
 
 def is_exceptional_name(name: str) -> bool:
     return name.startswith('APAC ') or 'Alternate art' in name or name.startswith('Avatar - ') or name.startswith('Euro ') or 'Reward Pack' in name
 
 def name_lookup(name: str) -> str:
-    if name == 'Kongming, Sleeping Dragon':
-        name = 'Kongming, "Sleeping Dragon"'
-    elif name == 'Pang Tong, Young Phoenix':
-        name = 'Pang Tong, "Young Phoenix"'
     try:
         if not CARDS:
             rs = db().select(multiverse.base_query())
@@ -62,6 +72,6 @@ def name_lookup(name: str) -> str:
     canonical = card.canonicalize(name)
     if canonical not in CARDS:
         if CARDS.get('', None) is None:
-            print('WARNING: Bogus name {name} ({canonical}) found.'.format(name=name, canonical=canonical))
+            logger.error(f'WARNING: Bogus name {name} ({canonical}) found.')
         return name
     return CARDS[canonical]

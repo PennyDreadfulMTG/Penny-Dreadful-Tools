7a0c8f44563c0622daa88e10b88e2bc1930cb749
Thomas David Baker <bakert@gmail.com>
2023-04-05 21:47:21 -0700

    Set the transaction isolation level for the session to get the benefit

--- decksite/data/preaggregation.py
+++ decksite/data/preaggregation.py
@@ -9,11 +9,12 @@ def preaggregate(table: str, sql: str) -> None:
         db().get_lock(lock_key, 60 * 5)
     except DatabaseException as e:
         logger.warning(f'Not preaggregating {table} because of {e}')
-    db().execute('SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED')
+    db().execute('SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED')
     db().execute(f'DROP TABLE IF EXISTS _new{table}')
     db().execute(sql)
     db().execute(f'DROP TABLE IF EXISTS _old{table}')
     db().execute(f'CREATE TABLE IF NOT EXISTS {table} (_ INT)')  # Prevent error in RENAME TABLE below if bootstrapping.
     db().execute(f'RENAME TABLE {table} TO _old{table}, _new{table} TO {table}')
     db().execute(f'DROP TABLE IF EXISTS _old{table}')
+    db().execute('SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ')
     db().release_lock(lock_key)

89a88e5ed3befe841f4ab826b7f33ffcfe66d344
Thomas David Baker <bakert@gmail.com>
2023-05-05 04:17:14 -0700

    Make test db a decorator in testutil

--- decksite/testutil.py
+++ decksite/testutil.py
@@ -1,9 +1,22 @@
+from functools import wraps
+
 from decksite import database
 from decksite.database import db
 from shared import configuration
 
 
-def setup_test_db() -> None:
+def with_test_db(test) -> None: # BAKERT return type
+    @wraps(test)
+    def wrapper(*args, **kwargs):
+        name = configuration.get_str('decksite_test_database')
+        configuration.CONFIG['decksite_database'] = name
+        db().execute(f'DROP DATABASE IF EXISTS {name}')
+        db().execute(f'CREATE DATABASE {name}')
+        db().execute(f'USE {name}')
+        database.setup()
+        test()
+    return wrapper
+
     name = configuration.get_str('decksite_test_database')
     configuration.CONFIG['decksite_database'] = name
     db().execute(f'DROP DATABASE IF EXISTS {name}')

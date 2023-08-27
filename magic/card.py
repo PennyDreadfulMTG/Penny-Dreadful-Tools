845b49e95c58ca89a6d66299684e9070a054ac85
Katelyn Gigante <clockwork.singularity@gmail.com>
2023-08-28 03:10:41 +1000

    lint

--- magic/card.py
+++ magic/card.py
@@ -72,8 +72,7 @@ def base_query_specific_properties() -> TableDescription:
     props['bugs']['type'] = TEXT
     props['bugs']['query'] = 'bugs'
     props['flavor_names']['type'] = TEXT
-    props['flavor_names']['query'] = "flavor_names"
-
+    props['flavor_names']['query'] = 'flavor_names'
     return props
 
 def card_properties() -> TableDescription:

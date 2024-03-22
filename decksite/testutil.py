7423a222a5294731fd3139b42caabb3341c67ac9
Thomas David Baker <bakert@gmail.com>
2024-03-22 16:01:53 -0700

    Test fixture should return db config to initial value after test

--- decksite/testutil.py
+++ decksite/testutil.py
@@ -9,6 +9,7 @@ from shared import configuration
 def with_test_db(test: Callable) -> Callable:
     @wraps(test)
     def wrapper(*args: Any, **kwargs: Any) -> None:
+        old_db_name = configuration.get_str('decksite_database')
         db_name = configuration.get_str('decksite_test_database')
         configuration.CONFIG['decksite_database'] = db_name
         db().execute(f'DROP DATABASE IF EXISTS {db_name}')
@@ -17,4 +18,5 @@ def with_test_db(test: Callable) -> Callable:
         database.setup()
         test(*args, **kwargs)
         db().execute(f'DROP DATABASE IF EXISTS {db_name}')
+        configuration.CONFIG['decksite_database'] = old_db_name
     return wrapper

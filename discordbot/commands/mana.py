835fcbe0ea7c0dcba6f2ddd6f234d1fce56c08a7
Thomas David Baker <bakert@gmail.com>
2024-04-25 12:51:15 -0700

    Fix /mana command

--- discordbot/commands/mana.py
+++ discordbot/commands/mana.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 
 from interactions.ext.prefixed_commands import prefixed_command
-from interactions.models import File, slash_command
+from interactions.models import slash_command
 
 from discordbot.command import MtgContext
 
@@ -10,8 +10,6 @@ from discordbot.command import MtgContext
 @slash_command('mana')
 async def mana(ctx: MtgContext) -> None:
     """Get Dr. Karsten's advice on number of colored sources of mana required."""
-    with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'img/mana-frank.png'), 'rb') as f:
-        img = File(f)
-        await ctx.send(file=img)
+    await ctx.send_image_with_retry(os.path.join(pathlib.Path(__file__).parent.absolute(), 'img/mana-frank.png'))
 
 m_mana = prefixed_command('mana')(mana.callback)

160c47d4a86379e84771af74f09b9e75212959ce
Thomas David Baker <bakert@gmail.com>
2024-04-26 13:10:31 -0700

    Make the message on /rotation make sense after rotation

--- decksite/views/rotation.py
+++ decksite/views/rotation.py
@@ -4,17 +4,17 @@ from shared import dtutil
 
 
 class Rotation(View):
-    def __init__(self, runs: int, num_cards: int) -> None:
+    def __init__(self, in_rotation: bool, runs: int, num_cards: int) -> None:
         super().__init__()
         self.runs = runs
         self.runs_percent = rotation.runs_percentage(runs)
         self.total_runs = rotation.TOTAL_RUNS
         self.num_cards = num_cards
-        if runs > 0:
-            self.rotation_msg = 'Rotation is in progress, ends ' + dtutil.display_date(seasons.next_rotation(), 2)
-            self.has_cards = True
-        else:
-            self.rotation_msg = 'Rotation is ' + dtutil.display_date(seasons.next_rotation(), 2)
+        self.has_cards = runs > 0
+        prefix = 'Rotation is in progress, ends ' if in_rotation else 'Rotation is '
+        display_date = dtutil.display_date(seasons.next_rotation(), 2)
+        self.rotation_msg = prefix + display_date
+        self.note = 'Data from the last rotation is shown' if runs and not in_rotation else ''
 
     def page_title(self) -> str:
         return 'Rotation'

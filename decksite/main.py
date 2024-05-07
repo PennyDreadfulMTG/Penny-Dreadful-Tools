765a11ba7f3b978b5cb847768036f8d6c1bc335a
Thomas David Baker <bakert@gmail.com>
2024-05-06 22:15:36 -0700

    Only load the number of cards we need on the homepage

--- decksite/main.py
+++ decksite/main.py
@@ -25,7 +25,8 @@ from shared.pd_exception import TooFewItemsException
 @cached()
 def home() -> str:
     decks = ds.latest_decks(season_id=get_season_id())
-    cards, _ = cs.load_cards(season_id=get_season_id())
+    top_8_plus_basics = 'LIMIT 13'
+    cards, total = cs.load_cards(limit=top_8_plus_basics, season_id=get_season_id())
     view = Home(ns.all_news(decks, max_items=10), decks, cards, ms.stats())
     return view.page()
 

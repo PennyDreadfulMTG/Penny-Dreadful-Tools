12fda9604c92c053cb51150e5ca3d448299c4607
Thomas David Baker <bakert@gmail.com>
2024-05-13 09:57:13 -0700

    Remove long-failing test
    Because whatsinstandard doesn't go back to S1 if we want to make these functions
    work from any start date we'd need to switch to some other method. We could use
    the seasons table in decksite but that would make this decksite-specific.
    
    In general it doesn't seem worth it to make a far-reaching change on how we
    think about seasons and pd500 dates and so on just to have some better tests so
    I'm removing this.

--- magic/tournaments_test.py
+++ magic/tournaments_test.py
@@ -1,9 +1,4 @@
-import datetime
-
-import freezegun
-import pytest
-
-from magic import seasons, tournaments
+from magic import tournaments
 from magic.models import Competition, Deck
 from shared import dtutil
 
@@ -12,18 +7,6 @@ COMPETITIONS = {
     'kick_off': Competition({'name': 'Penny Dreadful Kick Off (Season 99)'}),
     'normal': Competition({'name': 'Penny Dreadful Monday 99.99'}),
 }
-
-@pytest.mark.xfail(reason='The code this is testing will only ever rename the current season')
-def test_get_all_next_tournament_dates() -> None:
-    with freezegun.freeze_time('2021-02-01'):
-        seasons.rotation_info().recalculate()
-        just_before_s19_kick_off = datetime.datetime(2021, 2, 19).astimezone(tz=dtutil.WOTC_TZ)
-        just_after_s19_kick_off = datetime.datetime(2021, 2, 21).astimezone(tz=dtutil.WOTC_TZ)
-        info = tournaments.get_all_next_tournament_dates(just_before_s19_kick_off)
-        assert info[0][1] == 'Penny Dreadful FNM'
-        assert info[1][1] == 'Season Kick Off'
-        info = tournaments.get_all_next_tournament_dates(just_after_s19_kick_off)
-        assert info[-1][1] == 'Penny Dreadful Thursdays'
 
 def test_is_pd500() -> None:
     assert tournaments.is_pd500(COMPETITIONS['pd500'])

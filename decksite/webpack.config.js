1cc89e267304877aaaf9b14d899f14cda2c776df
Thomas David Baker <bakert@gmail.com>
2024-05-24 13:40:16 -0700

    Use source maps in dev js build for better debugging

--- decksite/webpack.config.js
+++ decksite/webpack.config.js
@@ -1,37 +1,40 @@
 /* eslint-disable */
-const webpack = require("webpack");
-const config = {
-    entry: [
-        __dirname + "/../shared_web/static/js/cardtable.jsx",
-        __dirname + "/../shared_web/static/js/decktable.jsx",
-        __dirname + "/../shared_web/static/js/headtoheadtable.jsx",
-        __dirname + "/../shared_web/static/js/leaderboardtable.jsx",
-        __dirname + "/../shared_web/static/js/matchtable.jsx",
-        __dirname + "/../shared_web/static/js/metagamegrid.jsx",
-        __dirname + "/../shared_web/static/js/persontable.jsx",
-        __dirname + "/../shared_web/static/js/rotationtable.jsx",
-    ],
-    output: {
-        path: __dirname + "/../shared_web/static/dist",
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".css"]
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?/,
-            exclude: /node_modules/,
-            use: "babel-loader"
-        }, {
-            test: /\.css$/,
-            exclude: /node_modules/,
-            use: [{
-                loader: 'style-loader'
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: [
+            __dirname + "/../shared_web/static/js/cardtable.jsx",
+            __dirname + "/../shared_web/static/js/decktable.jsx",
+            __dirname + "/../shared_web/static/js/headtoheadtable.jsx",
+            __dirname + "/../shared_web/static/js/leaderboardtable.jsx",
+            __dirname + "/../shared_web/static/js/matchtable.jsx",
+            __dirname + "/../shared_web/static/js/metagamegrid.jsx",
+            __dirname + "/../shared_web/static/js/persontable.jsx",
+            __dirname + "/../shared_web/static/js/rotationtable.jsx",
+        ],
+        output: {
+            path: __dirname + "/../shared_web/static/dist",
+            filename: "bundle.js"
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".css"]
+        },
+        module: {
+            rules: [{
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: "babel-loader"
             }, {
-                loader: 'css-loader'
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }]
             }]
-        }]
+        },
+        devtool: isProduction ? false : 'source-map'
     }
 };
-module.exports = config;

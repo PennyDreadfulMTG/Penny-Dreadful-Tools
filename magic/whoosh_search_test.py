d52dbb637a43de56866cb8d55e715ebd9204c4bc
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-09-07 18:45:41 +1000

    Longer sleeps in the modo-bugs forum scraper

--- magic/whoosh_search_test.py
+++ magic/whoosh_search_test.py
@@ -94,7 +94,7 @@ class WhooshSearchTest(unittest.TestCase):
     def test_meld(self) -> None:
         self.best_match_is('Graf Rats', 'Graf Rats')
         self.best_match_is('Midnight Scavengers', 'Midnight Scavengers')
-        self.best_match_is('Chittering Host', 'Midnight Scavengers')
+        self.best_match_is('Chittering Host', 'Graf Rats')
 
     def test_aliases(self) -> None:
         self.best_match_is('Jens', 'Solemn Simulacrum')

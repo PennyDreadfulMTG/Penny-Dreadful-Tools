1ceaf84be1a8f971048fe25c0fbf534470246cd2
Thomas David Baker <bakert@gmail.com>
2026-09-01 19:02:53 +0000

    Guide users to /rotation page when rotation is in progress (#12308)

--- discordbot/commands/rotation.py
+++ discordbot/commands/rotation.py
@@ -2,14 +2,17 @@ from interactions import Client, Extension
 from interactions.models import slash_command
 
 from discordbot.command import MtgContext
-from magic import seasons
+from magic import fetcher, rotation, seasons
 
 
 class Rotation(Extension):
     @slash_command()
     async def rotation(self, ctx: MtgContext) -> None:
         """Date of the next Penny Dreadful rotation."""
-        await ctx.send(seasons.message())
+        msg = seasons.message()
+        if rotation.in_rotation():
+            msg += f'\nSee what\'s changing: {fetcher.decksite_url("/rotation/")}'
+        await ctx.send(msg)
 
 def setup(bot: Client) -> None:
     Rotation(bot)

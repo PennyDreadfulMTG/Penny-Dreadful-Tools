b5128b6e60b397eee0e5251031b975fa19ba4cd4
Thomas David Baker <bakert@gmail.com>
2026-09-05 22:56:02 +0000

    Use emoji names only as a metadata fallback

--- decksite/views/metadata_test.py
+++ decksite/views/metadata_test.py
@@ -4,7 +4,7 @@ from decksite.views.deck import Deck as DeckView
 from shared.container import Container
 
 
-def test_deck_open_graph_metadata_replaces_emoji_with_words() -> None:
+def test_deck_open_graph_metadata_removes_emoji_from_mixed_text() -> None:
     view = DeckView.__new__(DeckView)
     view.deck = Container({
         'name': '🃏🔥',
@@ -16,12 +16,30 @@ def test_deck_open_graph_metadata_replaces_emoji_with_words() -> None:
     view.person_id = None
 
     assert view.og_title() == 'black joker fire'
-    assert view.og_description() == 'A deck by Pilot raised hand'
+    assert view.og_description() == 'A deck by Pilot'
 
 
-def test_archetype_open_graph_metadata_replaces_emoji_with_words() -> None:
+def test_archetype_open_graph_metadata_removes_emoji_from_mixed_text() -> None:
     view = ArchetypeView.__new__(ArchetypeView)
     view.archetype = Archetype(name='Burn🔥')
 
-    assert view.og_title() == 'Burn fire'
-    assert view.og_description() == 'Penny Dreadful Burn fire archetype'
+    assert view.og_title() == 'Burn'
+    assert view.og_description() == 'Penny Dreadful Burn archetype'
+
+
+def test_open_graph_metadata_uses_words_for_emoji_only_fields() -> None:
+    deck_view = DeckView.__new__(DeckView)
+    deck_view.deck = Container({
+        'name': '🔥',
+        'archetype_name': None,
+        'reviewed': False,
+        'person': '🐟',
+    })
+    deck_view.is_in_current_run = False
+    deck_view.person_id = None
+    archetype_view = ArchetypeView.__new__(ArchetypeView)
+    archetype_view.archetype = Archetype(name='🔥')
+
+    assert deck_view.og_title() == 'fire'
+    assert deck_view.og_description() == 'A deck by fish'
+    assert archetype_view.og_description() == 'Penny Dreadful fire archetype'

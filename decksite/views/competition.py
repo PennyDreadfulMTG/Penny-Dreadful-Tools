e53bc52c4a10e3b82d16ef3e5292545f81ab81ad
Thomas David Baker <bakert@gmail.com>
2026-09-06 13:59:05 +0000

    Show competition season and exact dates

--- decksite/views/competition.py
+++ decksite/views/competition.py
@@ -1,10 +1,18 @@
+import datetime
 from typing import Any
+
+from flask import url_for
 
 from decksite.data.archetype import Archetype
 from decksite.view import View
-from magic import tournaments
+from magic import seasons, tournaments
 from magic.models import Competition as Comp
 from shared import dtutil
+
+
+def _exact_date(date: datetime.datetime) -> str:
+    display = f'{date.astimezone(dtutil.WOTC_TZ):%b _%d_, %Y}'
+    return dtutil.replace_day_with_ordinal(display)
 
 
 class Competition(View):
@@ -15,12 +23,17 @@ class Competition(View):
         self.competition_id = self.competition.id
         self.hide_source = True
         self.has_external_source = competition.type != 'League'
+        self.competition_dates = _exact_date(competition.start_date)
         if competition.type == 'League':
             self.skinny_leaderboard = True  # Try and bunch it up on the right of decks table if at all possible.
             self.show_omw = True
             self.hide_top8 = True
             self.has_leaderboard = True
+            self.competition_dates = f'{self.competition_dates} – {_exact_date(competition.end_date)}'
         self.date = dtutil.display_date(competition.start_date)
+        if competition.season_id:
+            self.competition_season_name = seasons.season_name(competition.season_id)
+            self.competition_season_url = url_for('seasons.competitions', season_id=competition.season_id)
         self.archetypes = archetypes
         self.show_archetype_tree = len(self.archetypes) > 0
         self.hide_perfect_runs = self.tournament_only = competition.type != 'League'
@@ -29,6 +42,9 @@ class Competition(View):
 
     def __getattr__(self, attr: str) -> Any:
         return getattr(self.competition, attr)
+
+    def season_id(self) -> int:
+        return self.competition.season_id or super().season_id()
 
     def page_title(self) -> str:
         return self.competition.name
